59f7a5acf8c70d62d135a70be78b359cd99cf0e7
Žan Žibert <zzibert@plume.com>
2023-05-20 20:39:50 +0200

    Device actor with read and write messages

--- src/main/scala/com/example/Device.scala
+++ src/main/scala/com/example/Device.scala
@@ -1,5 +1,3 @@
-package com.example
-
 import akka.actor.typed.{ ActorRef, Behavior, PostStop, Signal }
 import akka.actor.typed.scaladsl.AbstractBehavior
 import akka.actor.typed.scaladsl.ActorContext
@@ -11,8 +9,13 @@ object Device {
     Behaviors.setup(context => new Device(context, groupId, deviceId))
 
   sealed trait Command
+
   final case class ReadTemperature(requestId: Long, replyTo: ActorRef[RespondTemperature]) extends Command
   final case class RespondTemperature(requestId: Long, value: Option[Double])
+
+  final case class RecordTemperature(requestId: Long, value: Double, replyTo: ActorRef[TemperatureRecorded])
+    extends Command
+  final case class TemperatureRecorded(requestId: Long)
 }
 
 class Device(context: ActorContext[Device.Command], groupId: String, deviceId: String)
@@ -25,6 +28,12 @@ class Device(context: ActorContext[Device.Command], groupId: String, deviceId: S
 
   override def onMessage(msg: Command): Behavior[Command] = {
     msg match {
+      case RecordTemperature(id, value, replyTo) =>
+        context.log.info2("Recorded temperature reading {} with {}", value, id)
+        lastTemperatureReading = Some(value)
+        replyTo ! TemperatureRecorded(id)
+        this
+
       case ReadTemperature(id, replyTo) =>
         replyTo ! RespondTemperature(id, lastTemperatureReading)
         this
