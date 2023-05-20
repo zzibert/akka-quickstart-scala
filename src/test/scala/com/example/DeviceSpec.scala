b1f9fb6cc4bbc57bf305522ffbf056f0f5460889
Žan Žibert <zzibert@plume.com>
2023-05-20 20:22:57 +0200

    Testing the Device Actor

---
+++ src/test/scala/com/example/DeviceSpec.scala
@@ -0,0 +1,21 @@
+package com.example
+
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class DeviceSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+
+  import Device._
+
+  "Device actor" must {
+    "reply with empty reading if no temperature is known" in {
+      val probe = createTestProbe[RespondTemperature]()
+      val deviceActor = spawn(Device("group", "device"))
+
+      deviceActor ! Device.ReadTemperature(requestId = 42, probe.ref)
+      val response = probe.receiveMessage()
+      response.requestId should ===(42)
+      response.value should ===(None)
+    }
+  }
+}
