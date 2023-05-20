0411f72912c2a46f0cbd79eceecbb50439706290
Žan Žibert <zzibert@plume.com>
2023-05-20 20:15:14 +0200

    Implementing the device actor and its read protocol

---
+++ src/main/scala/com/example/Device.scala
@@ -0,0 +1,40 @@
+package com.example
+
+import akka.actor.typed.{ ActorRef, Behavior, PostStop, Signal }
+import akka.actor.typed.scaladsl.AbstractBehavior
+import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.LoggerOps
+
+object Device {
+  def apply(groupId: String, deviceId: String): Behavior[Command] =
+    Behaviors.setup(context => new Device(context, groupId, deviceId))
+
+  sealed trait Command
+  final case class ReadTemperature(requestId: Long, replyTo: ActorRef[RespondTemperature]) extends Command
+  final case class RespondTemperature(requestId: Long, value: Option[Double])
+}
+
+class Device(context: ActorContext[Device.Command], groupId: String, deviceId: String)
+  extends AbstractBehavior[Device.Command](context) {
+  import Device._
+
+  var lastTemperatureReading: Option[Double] = None
+
+  context.log.info2("Device actor {}-{} started", groupId, deviceId)
+
+  override def onMessage(msg: Command): Behavior[Command] = {
+    msg match {
+      case ReadTemperature(id, replyTo) =>
+        replyTo ! RespondTemperature(id, lastTemperatureReading)
+        this
+    }
+  }
+
+  override def onSignal: PartialFunction[Signal, Behavior[Command]] = {
+    case PostStop =>
+      context.log.info2("Device actor {}-{} stopped", groupId, deviceId)
+      this
+  }
+
+}
---
+++ src/main/scala/com/example/IotApp.scala
@@ -0,0 +1,12 @@
+package com.example
+
+import akka.actor.typed.ActorSystem
+
+object IotApp {
+
+  def main(args: Array[String]): Unit = {
+    // Create ActorSystem and top level supervisor
+    ActorSystem[Nothing](IotSupervisor(), "iot-system")
+  }
+
+}
