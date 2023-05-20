02c4a7f181ec7c570d378c0ec72ebaabaa6a4b38
Žan Žibert <zzibert@plume.com>
2023-05-20 18:39:34 +0200

    Restart supervision strategy

--- src/main/scala/com/example/ActorHierarchyExperiments.scala
+++ src/main/scala/com/example/ActorHierarchyExperiments.scala
@@ -1,23 +1,7 @@
 package com.example
 
 import akka.actor.typed.scaladsl.{ AbstractBehavior, ActorContext, Behaviors }
-import akka.actor.typed.{ ActorSystem, Behavior, PostStop, Signal }
-
-object PrintMyActorRefActor {
-  def apply(): Behavior[String] =
-    Behaviors.setup(context => new PrintMyActorRefActor(context))
-}
-
-class PrintMyActorRefActor(context: ActorContext[String]) extends AbstractBehavior[String](context) {
-
-  override def onMessage(msg: String): Behavior[String] =
-    msg match {
-      case "printit" =>
-        val secondRef = context.spawn(Behaviors.empty[String], "second-actor")
-        println(s"Second: $secondRef")
-        this
-    }
-}
+import akka.actor.typed.{ ActorSystem, Behavior, PostStop, PreRestart, Signal, SupervisorStrategy }
 
 object Main {
   def apply(): Behavior[String] =
@@ -29,8 +13,8 @@ class Main(context: ActorContext[String]) extends AbstractBehavior[String](conte
   override def onMessage(msg: String): Behavior[String] =
     msg match {
       case "start" =>
-        val first = context.spawn(StartStopActor1(), "first")
-        first ! "stop"
+        val supervisingActor = context.spawn(SupervisingActor(), "supervising-actor")
+        supervisingActor ! "failChild"
         this
     }
 }
@@ -40,45 +24,48 @@ object ActorHierarchyExperiments extends App {
   testSystem ! "start"
 }
 
-object StartStopActor1 {
+object SupervisingActor {
   def apply(): Behavior[String] =
-    Behaviors.setup(context => new StartStopActor1(context))
+    Behaviors.setup(context => new SupervisingActor(context))
 }
 
-class StartStopActor1(context: ActorContext[String]) extends AbstractBehavior[String](context) {
-  println("first started")
-  context.spawn(StartStopActor2(), "second")
+class SupervisingActor(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+  private val child = context.spawn(
+    Behaviors.supervise(SupervisedActor()).onFailure(SupervisorStrategy.restart),
+    name = "supervised-actor")
 
   override def onMessage(msg: String): Behavior[String] =
     msg match {
-      case "stop" => Behaviors.stopped
+      case "failChild" =>
+        child ! "fail"
+        this
+    }
+}
+
+object SupervisedActor {
+  def apply(): Behavior[String] =
+    Behaviors.setup(context => new SupervisedActor(context))
+}
+
+class SupervisedActor(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+  println("supervised actor started")
+
+  override def onMessage(msg: String): Behavior[String] =
+    msg match {
+      case "fail" =>
+        println("supervised actor fails now")
+        throw new Exception("I failed!")
     }
 
   override def onSignal: PartialFunction[Signal, Behavior[String]] = {
+    case PreRestart =>
+      println("supervised actor will be restarted")
+      this
     case PostStop =>
-      println("first stopped")
+      println("supervised actor stopped")
       this
   }
 
 }
 
-object StartStopActor2 {
-  def apply(): Behavior[String] =
-    Behaviors.setup(new StartStopActor2(_))
-}
 
-class StartStopActor2(context: ActorContext[String]) extends AbstractBehavior[String](context) {
-  println("second started")
-
-  override def onMessage(msg: String): Behavior[String] = {
-    // no messages handled by this actor
-    Behaviors.unhandled
-  }
-
-  override def onSignal: PartialFunction[Signal, Behavior[String]] = {
-    case PostStop =>
-      println("second stopped")
-      this
-  }
-
-}
