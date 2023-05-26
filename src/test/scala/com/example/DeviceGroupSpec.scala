a629fc79becc703e8ae6964bda6315d8cda78ec7
Žan Žibert <zzibert@plume.com>
2023-05-26 16:26:51 +0200

    Complete deviceGroupActor

--- src/test/scala/com/example/DeviceGroupSpec.scala
+++ src/test/scala/com/example/DeviceGroupSpec.scala
@@ -1,8 +1,10 @@
 package com.example
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
-import com.example.Device.Passivate
+import com.example.Device.{ Passivate, RecordTemperature, TemperatureRecorded }
+import com.example.DeviceManager.{ DeviceRegistered, ReplyDeviceList, RequestAllTemperatures, RequestDeviceList, RequestTrackDevice, RespondAllTemperatures, Temperature, TemperatureNotAvailable }
 import org.scalatest.wordspec.AnyWordSpecLike
+import scala.concurrent.duration.DurationInt
 
 class DeviceGroupSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
   "be able to register a device actor" in {
@@ -88,5 +90,35 @@ class DeviceGroupSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
       groupActor ! RequestDeviceList(requestId = 1, groupId = "group", deviceListProbe.ref)
       deviceListProbe.expectMessage(ReplyDeviceList(requestId = 1, Set("device2")))
     }
+  }
+
+  "be able to collect temperatures from all active devices" in {
+    val registeredProbe = createTestProbe[DeviceRegistered]()
+    val groupActor = spawn(DeviceGroup("group"))
+
+    groupActor ! RequestTrackDevice("group", "device1", registeredProbe.ref)
+    val deviceActor1 = registeredProbe.receiveMessage().device
+
+    groupActor ! RequestTrackDevice("group", "device2", registeredProbe.ref)
+    val deviceActor2 = registeredProbe.receiveMessage().device
+
+    groupActor ! RequestTrackDevice("group", "device3", registeredProbe.ref)
+    registeredProbe.receiveMessage()
+
+    // Check that the device actors are working
+    val recordProbe = createTestProbe[TemperatureRecorded]()
+    deviceActor1 ! RecordTemperature(requestId = 0, 1.0, recordProbe.ref)
+    recordProbe.expectMessage(TemperatureRecorded(requestId = 0))
+    deviceActor2 ! RecordTemperature(requestId = 1, 2.0, recordProbe.ref)
+    recordProbe.expectMessage(TemperatureRecorded(requestId = 1))
+    // No temperature for device3
+
+    val allTempProbe = createTestProbe[RespondAllTemperatures]()
+    groupActor ! RequestAllTemperatures(requestId = 0, groupId = "group", allTempProbe.ref)
+    allTempProbe.expectMessage(
+      RespondAllTemperatures(
+        requestId = 0,
+        temperatures =
+          Map("device1" -> Temperature(1.0), "device2" -> Temperature(2.0), "device3" -> TemperatureNotAvailable)))
   }
 }
