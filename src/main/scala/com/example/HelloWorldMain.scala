b9273c7137a0b93001d896f0f710ffcbe880f7f2
Žan Žibert <zzibert@plume.com>
2023-05-29 15:24:23 +0200

    HelloWorld Main actor

---
+++ src/main/scala/com/example/HelloWorldMain.scala
@@ -0,0 +1,22 @@
+package com.example
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.LoggerOps
+import akka.actor.typed.{ ActorRef, ActorSystem, Behavior }
+
+object HelloWorldMain {
+
+  final case class SayHello(name: String)
+
+  def apply(): Behavior[SayHello] =
+    Behaviors.setup { context =>
+      val greeter = context.spawn(HelloWorld(), "greeter")
+
+      Behaviors.receiveMessage { message =>
+        val replyTo = context.spawn(HelloWorldBot(max = 3), message.name)
+        greeter ! HelloWorld.Greet(message.name, replyTo)
+        Behaviors.same
+      }
+    }
+
+}
