7d08573422851dd55336111a336acc30b6779f4a
Žan Žibert <zzibert@plume.com>
2023-05-29 15:09:12 +0200

    HelloWorld typed Actor

---
+++ src/main/scala/com/example/HelloWorld.scala
@@ -0,0 +1,16 @@
+package com.example
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.LoggerOps
+import akka.actor.typed.{ ActorRef, ActorSystem, Behavior }
+
+object HelloWorld {
+  final case class Greet(whom: String, replyTo: ActorRef[Greeted])
+  final case class Greeted(whom: String, from: ActorRef[Greet])
+
+  def apply(): Behavior[Greet] = Behaviors.receive { (context, message) =>
+    context.log.info("Hello {}!", message.whom)
+    message.replyTo ! Greeted(message.whom, context.self)
+    Behaviors.same
+  }
+}
