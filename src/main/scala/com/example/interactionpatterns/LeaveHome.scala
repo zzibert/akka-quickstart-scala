52c0299e7444fe78d0451ded675bbe519825dd2a
Žan Žibert <zzibert@plume.com>
2023-06-25 11:17:32 +0200

    Response Aggregator pattern

---
+++ src/main/scala/com/example/interactionpatterns/LeaveHome.scala
@@ -0,0 +1,72 @@
+package com.example.interactionpatterns
+
+import akka.NotUsed
+import akka.actor.typed.scaladsl.Behaviors
+
+// dummy data types just for this sample
+case class Keys()
+
+case class Wallet()
+
+
+object Home {
+  sealed trait Command
+  case class LeaveHome(who: String, replyTo: ActorRef[ReadyToLeaveHome]) extends Command
+  case class ReadyToLeaveHome(who: String, keys: Keys, wallet: Wallet)
+
+  def apply(): Behavior[Command] = {
+    Behaviors.setup[Command] { context =>
+      val keyCabinet: ActorRef[KeyCabinet.GetKeys] = context.spawn(KeyCabinet(), "key-cabinet")
+      val drawer: ActorRef[Drawer.GetWallet] = context.spawn(Drawer(), "drawer")
+
+      Behaviors.receiveMessage[Command] {
+        case LeaveHome(who, replyTo) =>
+          context.spawn(prepareToLeaveHome(who, replyTo, keyCabinet, drawer), s"leaving-$who")
+          Behaviors.same
+      }
+    }
+  }
+
+  // per session actor behavior
+  def prepareToLeaveHome(
+                          whoIsLeaving: String,
+                          replyTo: ActorRef[ReadyToLeaveHome],
+                          keyCabinet: ActorRef[KeyCabinet.GetKeys],
+                          drawer: ActorRef[Drawer.GetWallet]): Behavior[NotUsed] = {
+    // we don't _really_ care about the actor protocol here as nobody will send us
+    // messages except for responses to our queries, so we just accept any kind of message
+    // but narrow that to more limited types when we interact
+    Behaviors
+      .setup[AnyRef] { context =>
+        var wallet: Option[Wallet] = None
+        var keys: Option[Keys] = None
+
+        // we narrow the ActorRef type to any subtype of the actual type we accept
+        keyCabinet ! KeyCabinet.GetKeys(whoIsLeaving, context.self.narrow[Keys])
+        drawer ! Drawer.GetWallet(whoIsLeaving, context.self.narrow[Wallet])
+
+        def nextBehavior(): Behavior[AnyRef] =
+          (keys, wallet) match {
+            case (Some(w), Some(k)) =>
+              // we got both, "session" is completed!
+              replyTo ! ReadyToLeaveHome(whoIsLeaving, w, k)
+              Behaviors.stopped
+
+            case _ =>
+              Behaviors.same
+          }
+
+        Behaviors.receiveMessage {
+          case w: Wallet =>
+            wallet = Some(w)
+            nextBehavior()
+          case k: Keys =>
+            keys = Some(k)
+            nextBehavior()
+          case _ =>
+            Behaviors.unhandled
+        }
+      }
+      .narrow[NotUsed] // we don't let anyone else know we accept anything
+  }
+}
---
+++ src/main/scala/com/example/interactionpatterns/ResponseAggregator.scala
@@ -0,0 +1,56 @@
+package com.example.interactionpatterns
+
+import akka.actor.typed.{ ActorRef, Behavior }
+import akka.actor.typed.scaladsl.Behaviors
+import scala.concurrent.duration.DurationInt
+
+object Hotel1 {
+  final case class RequestQuote(replyTo: ActorRef[Quote])
+  final case class Quote(hotel: String, price: BigDecimal)
+}
+object Hotel2 {
+  final case class RequestPrice(replyTo: ActorRef[Price])
+  final case class Price(hotel: String, price: BigDecimal)
+}
+
+// Any since no common type between Hotel1 and Hotel2
+type Reply = Any
+
+object HotelCustomer {
+  sealed trait Command
+  final case class Quote(hotel: String, price: BigDecimal)
+  final case class AggregatedQuotes(quotes: List[Quote]) extends Command
+
+  def apply(hotel1: ActorRef[Hotel1.RequestQuote], hotel2: ActorRef[Hotel2.RequestPrice]): Behavior[Command] = {
+
+    Behaviors.setup[Command] { context =>
+      context.spawnAnonymous(
+        Aggregator[Reply, AggregatedQuotes](
+          sendRequests = { replyTo =>
+            hotel1 ! Hotel1.RequestQuote(replyTo)
+            hotel2 ! Hotel2.RequestPrice(replyTo)
+          },
+          expectedReplies = 2,
+          context.self,
+          aggregateReplies = replies =>
+            // The hotels have different protocols with different replies,
+            // convert them to `HotelCustomer.Quote` that this actor understands.
+            AggregatedQuotes(
+              replies
+                .map {
+                  case Hotel1.Quote(hotel, price) => Quote(hotel, price)
+                  case Hotel2.Price(hotel, price) => Quote(hotel, price)
+                  case unknown                    => throw new RuntimeException(s"Unknown reply $unknown")
+                }
+                .sortBy(_.price)
+                .toList),
+          timeout = 5.seconds))
+
+      Behaviors.receiveMessage {
+        case AggregatedQuotes(quotes) =>
+          context.log.info("Best {}", quotes.headOption.getOrElse("Quote N/A"))
+          Behaviors.same
+      }
+    }
+  }
+}
