7f87895e4ce612eb717626637b7699d1132d6c98
Žan Žibert <zzibert@plume.com>
2023-06-25 08:16:02 +0200

    Cookie fabric with StatusReply pattern

--- src/main/scala/com/example/interactionpatterns/CookieFabric.scala
+++ src/main/scala/com/example/interactionpatterns/CookieFabric.scala
@@ -3,24 +3,22 @@ package com.example.interactionpatterns
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.LoggerOps
 import akka.actor.typed.{ ActorRef, ActorSystem, Behavior, PostStop, Terminated }
+import akka.pattern.StatusReply
 import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
 import scala.util.{ Failure, Success }
 
 object CookieFabric {
   sealed trait Command
-  case class GiveMeCookies(count: Int, replyTo: ActorRef[Reply]) extends Command
-
-  sealed trait Reply
-  case class Cookies(count: Int) extends Reply
-  case class InvalidRequest(reason: String) extends Reply
+  case class GiveMeCookies(count: Int, replyTo: ActorRef[StatusReply[Cookies]]) extends Command
+  case class Cookies(count: Int)
 
   def apply(): Behaviors.Receive[CookieFabric.GiveMeCookies] =
     Behaviors.receiveMessage { message =>
       if (message.count >= 5)
-        message.replyTo ! InvalidRequest("Too many cookies.")
+        message.replyTo ! StatusReply.Error("Too many cookies.")
       else
-        message.replyTo ! Cookies(message.count)
+        message.replyTo ! StatusReply.Success(Cookies(message.count))
       Behaviors.same
     }
 }
@@ -34,13 +32,13 @@ implicit val timeout: Timeout = 3.seconds
 // implicit ActorSystem in scope
 implicit val system: ActorSystem[_] = theSystem
 
-val result: Future[CookieFabric.Reply] = cookieFabric.ask(ref => CookieFabric.GiveMeCookies(3, ref))
+val result: Future[CookieFabric.Cookies] = cookieFabric.askWithStatus(ref => CookieFabric.GiveMeCookies(3, ref))
 
 // the response callback will be executed on this execution context
 implicit val ec = system.executionContext
 
 result.onComplete {
-  case Success(CookieFabric.Cookies(count))         => println(s"Yay, $count cookies!")
-  case Success(CookieFabric.InvalidRequest(reason)) => println(s"No cookies for me. $reason")
-  case Failure(ex)                                  => println(s"Boo! didn't get cookies: ${ex.getMessage}")
+  case Success(CookieFabric.Cookies(count))      => println(s"Yay, $count cookies!")
+  case Failure(StatusReply.ErrorMessage(reason)) => println(s"No cookies for me. $reason")
+  case Failure(ex)                               => println(s"Boo! didn't get cookies: ${ex.getMessage}")
 }
