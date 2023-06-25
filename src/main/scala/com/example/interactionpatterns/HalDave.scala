dd4f3c67ffce025b25641ba4e4b419ba54539fd3
Žan Žibert <zzibert@plume.com>
2023-06-25 08:06:28 +0200

    HalDave with StatusReply pattern

--- src/main/scala/com/example/interactionpatterns/HalDave.scala
+++ src/main/scala/com/example/interactionpatterns/HalDave.scala
@@ -3,19 +3,20 @@ package com.example.interactionpatterns
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.scaladsl.LoggerOps
 import akka.actor.typed.{ ActorRef, ActorSystem, Behavior, PostStop, Terminated }
+import akka.pattern.StatusReply
 import akka.util.Timeout
 import scala.concurrent.duration.DurationInt
 import scala.util.{ Failure, Success }
 
 object Hal {
   sealed trait Command
-  case class OpenThePodBayDoorsPlease(replyTo: ActorRef[Response]) extends Command
-  case class Response(message: String)
+  case class OpenThePodBayDoorsPlease(replyTo: ActorRef[StatusReply[String]]) extends Command
 
   def apply(): Behaviors.Receive[Hal.Command] =
     Behaviors.receiveMessage[Command] {
       case OpenThePodBayDoorsPlease(replyTo) =>
-        replyTo ! Response("I'm sorry, Dave. I'm afraid I can't do that.")
+        // reply with a validation error description
+        replyTo ! StatusReply.Error("I'm sorry, Dave. I'm afraid I can't do that.")
         Behaviors.same
     }
 }
@@ -32,23 +33,12 @@ object Dave {
       // the ask is failed with a TimeoutException
       implicit val timeout: Timeout = 3.seconds
 
-      // Note: The second parameter list takes a function `ActorRef[T] => Message`,
-      // as OpenThePodBayDoorsPlease is a case class it has a factory apply method
-      // that is what we are passing as the second parameter here it could also be written
-      // as `ref => OpenThePodBayDoorsPlease(ref)`
-      context.ask(hal, Hal.OpenThePodBayDoorsPlease.apply) {
-        case Success(Hal.Response(message)) => AdaptedResponse(message)
-        case Failure(_)                     => AdaptedResponse("Request failed")
-      }
-
-      // we can also tie in request context into an interaction, it is safe to look at
-      // actor internal state from the transformation function, but remember that it may have
-      // changed at the time the response arrives and the transformation is done, best is to
-      // use immutable state we have closed over like here.
-      val requestId = 1
-      context.ask(hal, Hal.OpenThePodBayDoorsPlease.apply) {
-        case Success(Hal.Response(message)) => AdaptedResponse(s"$requestId: $message")
-        case Failure(_)                     => AdaptedResponse(s"$requestId: Request failed")
+      // A StatusReply.Success(m) ends up as a Success(m) here, while a
+      // StatusReply.Error(text) becomes a Failure(ErrorMessage(text))
+      context.askWithStatus(hal, Hal.OpenThePodBayDoorsPlease.apply) {
+        case Success(message)                        => AdaptedResponse(message)
+        case Failure(StatusReply.ErrorMessage(text)) => AdaptedResponse(s"Request denied: $text")
+        case Failure(_)                              => AdaptedResponse("Request failed")
       }
 
       Behaviors.receiveMessage {
