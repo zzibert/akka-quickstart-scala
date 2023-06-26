a7673e67e7b9b38cbfc98665741b226ea98505c6
Žan Žibert <zzibert@plume.com>
2023-06-26 10:16:31 +0200

    supervision strategy inside the setup to not recreate children on restart

--- src/main/scala/com/example/faulttolerance/ParentRestarting.scala
+++ src/main/scala/com/example/faulttolerance/ParentRestarting.scala
@@ -24,3 +24,23 @@ def parent: Behavior[String] = {
     }
     .onFailure(SupervisorStrategy.restart)
 }
+
+def parent2: Behavior[String] = {
+  Behaviors.setup { ctx =>
+    val child1 = ctx.spawn(child(0), "child1")
+    val child2 = ctx.spawn(child(0), "child2")
+
+    // supervision strategy inside the setup to not recreate children on restart
+    Behaviors
+      .supervise {
+        Behaviors.receiveMessage[String] { msg =>
+          // message handling that might throw an exception
+          val parts = msg.split(" ")
+          child1 ! parts(0)
+          child2 ! parts(1)
+          Behaviors.same
+        }
+      }
+      .onFailure(SupervisorStrategy.restart.withStopChildren(false))
+  }
+}
