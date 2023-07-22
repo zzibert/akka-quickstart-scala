9baa2bdf8e80ce703ee817dc82833819f5f8a23b
Žan Žibert <zzibert@plume.com>
2023-07-22 10:04:35 +0200

    Meetings Rooms #252 with sorting

--- src/main/scala/leetcode/MeetingRooms.scala
+++ src/main/scala/leetcode/MeetingRooms.scala
@@ -1,19 +1,17 @@
 package leetcode
 
-import scala.collection.mutable
+
 
 class MeetingRooms {
   def canAttendMeetings(intervals: Array[Array[Int]]): Boolean = {
-    val attendance = mutable.Map[Int, Boolean]()
+    val sorted = intervals.sortBy(_(0))
 
-    for (interval <- intervals) {
-      for (hour <- (interval(0) until interval(1))) {
-        if (attendance.getOrElse(hour, false)) {
-          return false
-        }
-        attendance(hour) = true
+    for (i <- 0 until (sorted.length - 1)) {
+      if (sorted(i)(1) > sorted(i + 1)(0)) {
+        return false
       }
     }
+
     true
   }
 }
