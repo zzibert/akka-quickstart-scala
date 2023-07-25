273e599fd9bf39dca7540b95d2cba1c91dcf40a8
Žan Žibert <zzibert@plume.com>
2023-07-25 22:45:24 +0200

    Flood Fill recursive

--- src/main/scala/leetcode/FloodFill.scala
+++ src/main/scala/leetcode/FloodFill.scala
@@ -6,31 +6,23 @@ import scala.collection.mutable
 object FloodFill {
   def floodFill(image: Array[Array[Int]], sr: Int, sc: Int, color: Int): Array[Array[Int]] = {
     val target = image(sr)(sc)
-    val queue = mutable.Queue[(Int, Int)]((sr, sc))
     val alreadyVisited = mutable.Map[(Int, Int), Boolean]()
 
-    while(queue.nonEmpty) {
-      val (row, column) = queue.dequeue()
-      alreadyVisited += ((row, column) -> true)
-      if (floodFillHelper(image, row, column, color, target, alreadyVisited)) {
-        queue.enqueue((row-1, column))
-        queue.enqueue((row, column+1))
-        queue.enqueue((row+1, column))
-        queue.enqueue((row, column-1))
-      }
-    }
+    floodFillHelper(image, sr, sc, color, target, alreadyVisited)
 
     image
   }
 
-  def floodFillHelper(image: Array[Array[Int]], sr: Int, sc: Int, color: Int, target: Int, visited: mutable.Map[(Int, Int), Boolean]): Boolean = {
+  def floodFillHelper(image: Array[Array[Int]], sr: Int, sc: Int, color: Int, target: Int, visited: mutable.Map[(Int, Int), Boolean]): Unit = {
     if (!visited.getOrElse((sr, sc), false) && image.length > sr && sr >= 0 && image(0).length > sc && sc >= 0) {
       if (image(sr)(sc) == target) {
         image(sr)(sc) = color
         visited += ((sr, sc) -> true)
-        return true
+        floodFillHelper(image, sr-1, sc, color, target, visited)
+        floodFillHelper(image, sr, sc+1, color, target, visited)
+        floodFillHelper(image, sr+1, sc, color, target, visited)
+        floodFillHelper(image, sr, sc-1, color, target, visited)
       }
     }
-    false
   }
 }
