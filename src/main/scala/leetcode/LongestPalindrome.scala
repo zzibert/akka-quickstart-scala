fa7d9ddfab6033ddb3e99e1d6b71efabc2e50e6b
Žan Žibert <zzibert@plume.com>
2023-08-05 22:17:00 +0200

    Longest Palindrome #409

--- src/main/scala/leetcode/LongestPalindrome.scala
+++ src/main/scala/leetcode/LongestPalindrome.scala
@@ -1,3 +1,31 @@
-package leetcode class LongestPalindrome {
+package leetcode
 
+import scala.collection.mutable
+
+object LongestPalindrome {
+  def longestPalindrome(s: String): Int = {
+    val letters = mutable.Map[Char, Int]()
+    var longest = 0
+    var _addOne = false
+
+    s foreach { c =>
+      letters.getOrElseUpdate(c, 0)
+      letters(c) += 1
+    }
+
+    letters.values foreach { count =>
+      if (count % 2 == 0) {
+        longest += count
+      } else {
+        longest += (count - 1)
+        _addOne = true
+      }
+    }
+
+    if (_addOne) {
+      longest + 1
+    } else {
+      longest
+    }
+  }
 }
