50f6fcc15f28e73b0779c6a047ac9b8509634f3c
Žan Žibert <zzibert@plume.com>
2023-08-05 23:08:02 +0200

    Lowest Common Ancestor of a Binary Tree #236

--- src/main/scala/leetcode/LowestCommonAncestorOfABinarySearchTree.scala
+++ src/main/scala/leetcode/LowestCommonAncestorOfABinarySearchTree.scala
@@ -9,19 +9,28 @@ class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null)
 
 object LowestCommonAncestorOfABinarySearchTree {
   def lowestCommonAncestor(root: TreeNode, p: TreeNode, q: TreeNode): TreeNode = {
-    val pTraversal = traverseTo(root, p, Seq[TreeNode]())
-    val qTraversal = traverseTo(root, q, Seq[TreeNode]())
+    val pPath = getPath(root, p.value)
+    val qPath = getPath(root, q.value)
 
-    pTraversal.intersect(qTraversal).head
+    pPath.intersect(qPath).last
+
   }
-
-  def traverseTo(root: TreeNode, target: TreeNode, chain: Seq[TreeNode]): Seq[TreeNode] = {
-    if (root.value == target.value) {
-      root +: chain
-    } else if (root.value < target.value) {
-      traverseTo(root.right, target, root +: chain)
+  def getPath(root: TreeNode, value: Int): List[TreeNode] = {
+    if (root == null) {
+      Nil
+    } else if (root.value == value) {
+      List(root)
     } else {
-      traverseTo(root.left, target, root +: chain)
+      val left = getPath(root.left, value)
+      val right = getPath(root.right, value)
+
+      if (left.isEmpty && right.isEmpty) {
+        Nil
+      } else if (left.isEmpty) {
+        root :: right
+      } else {
+        root :: left
+      }
     }
   }
 }
