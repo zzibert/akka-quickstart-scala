a067f56a566e6f11dcb37cc57266a461aaf86dbe
Žan Žibert <zzibert@plume.com>
2023-08-06 16:15:33 +0200

    Longest Common Prefix #14

--- src/main/scala/leetcode/LongestCommonPrefix.scala
+++ src/main/scala/leetcode/LongestCommonPrefix.scala
@@ -1,3 +1,39 @@
-package leetcode class LongestCommonPrefix {
+package leetcode
 
+import scala.collection.mutable
+
+
+class LongestCommonPrefix {
+  def longestCommonPrefix(strs: Array[String]): String = {
+    val letters = mutable.Map[Int, mutable.Set[Char]]()
+    var result = ""
+    var shortest = Int.MaxValue
+
+    strs foreach { str =>
+      if (str.length < shortest) {
+        shortest = str.length
+      }
+    }
+
+    strs foreach { str =>
+      str.zipWithIndex foreach {
+        case (c, i) =>
+          val s = letters.getOrElseUpdate(i, mutable.Set[Char]())
+          s.add(c)
+      }
+    }
+
+    val length = letters.keys.toArray.length
+
+    for (i <- 0 until shortest) {
+      val s = letters(i).toArray
+      if (s.length == 1) {
+        result += s(0)
+      } else {
+         return result
+      }
+    }
+
+    result
+  }
 }
