af6b299878574789df67f1cb309f709c242fd096
Žan Žibert <zzibert@plume.com>
2023-08-12 19:05:39 +0200

    Passes 308 from 312 test cases for 3sum #15

--- src/main/scala/leetcode/Sum3.scala
+++ src/main/scala/leetcode/Sum3.scala
@@ -7,7 +7,6 @@ object Sum3 {
   def threeSum(nums: Array[Int]): List[List[Int]] = {
     val sums = mutable.Map[Int, ListBuffer[(Int, Int)]]()
     val results = mutable.ArrayBuffer[List[Int]]()
-    val indexes = mutable.ArrayBuffer[List[Int]]()
 
     for {
       i <- 0 until (nums.length-1)
@@ -23,18 +22,12 @@ object Sum3 {
         sums.getOrElse(0 - number, ListBuffer.empty) foreach {
           case (i, j) =>
             if (index != i && index != j) {
-              val indexResult = List(index, i, j).sorted
-              if (!indexes.exists(_ == indexResult)) {
-                indexes.addOne(indexResult)
-                val result = List(number, nums(i), nums(j)).sorted
-                if (!results.exists(_ == result)) {
-                  results.addOne(result)
-                }
-              }
+              val result = List(number, nums(i), nums(j)).sorted
+              results.addOne(result)
             }
         }
     }
 
-    results.toList.distinct
+    results.distinct.toList
   }
 }
