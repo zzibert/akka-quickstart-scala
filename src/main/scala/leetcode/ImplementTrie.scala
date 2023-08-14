c6c47b3e96215c80ba184c29b850b12a7c3b131c
Žan Žibert <zzibert@plume.com>
2023-08-14 11:59:34 +0200

    Implement Trie #208

---
+++ src/main/scala/leetcode/ImplementTrie.scala
@@ -0,0 +1,62 @@
+package leetcode
+
+import scala.collection.mutable
+
+object ImplementTrie {
+  class Trie() {
+    var value: Char = '0'
+    var isWord: Boolean = false
+    var children = mutable.Set[Trie]()
+
+    def insert(word: String) {
+      if (word != "") {
+        val head = word.head
+        val tail = word.tail
+
+        children.find(_.value == head) match {
+          case Some(child) =>
+            child.insert(tail)
+
+          case None =>
+            val child = new Trie()
+            child.value = head
+            children.addOne(child)
+            child.insert(tail)
+        }
+      } else {
+        isWord = true
+      }
+    }
+
+    def search(word: String): Boolean = {
+      if (word == "") {
+        isWord
+      } else {
+        val head = word.head
+        val tail = word.tail
+        children.find(_.value == head) match {
+          case Some(child) =>
+            child.search(tail)
+          case None =>
+            false
+        }
+      }
+    }
+
+    def startsWith(prefix: String): Boolean = {
+      if (prefix == "") {
+        true
+      } else {
+        val head = prefix.head
+        val tail = prefix.tail
+        children.find(_.value == head) match {
+          case Some(child) =>
+            child.startsWith(tail)
+          case None =>
+            false
+        }
+      }
+    }
+
+  }
+}
