bf1570cb14a24fcdccc845842b61e4cd637ac351
Žan Žibert <zzibert@plume.com>
2023-08-15 21:06:07 +0200

    Validate Binary Search Tree #98 - Solves 75 out of 83 test cases

--- src/main/scala/leetcode/ValidateBinarySearchTree.scala
+++ src/main/scala/leetcode/ValidateBinarySearchTree.scala
@@ -1,3 +1,55 @@
-package leetcode class ValidateBinarySearchTree {
+package leetcode
+
+class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+  var value: Int = _value
+  var left: TreeNode = _left
+  var right: TreeNode = _right
+}
+
+object ValidateBinarySearchTree {
+  def isValidBST(root: TreeNode): Boolean = {
+
+    isValidBSTHelper(root,maxValue = Int.MaxValue, minValue = Int.MinValue)
+  }
+
+  def isValidBSTHelper(root: TreeNode, maxValue: Int, minValue: Int): Boolean = {
+    if (root == null) {
+      return true
+    }
+
+    if (root.value <= minValue || root.value >= maxValue) {
+      return false
+    }
+
+    val leftMin =
+      if (root.value < minValue) {
+        root.value
+      } else {
+        minValue
+      }
+
+    val leftMax =
+      if (root.value < maxValue) {
+        root.value
+      } else {
+        maxValue
+      }
+
+    val rightMin =
+      if (root.value > minValue) {
+        root.value
+      } else {
+        minValue
+      }
+
+    val rightMax =
+      if (root.value > maxValue) {
+        root.value
+      } else {
+        maxValue
+      }
+
+    isValidBSTHelper(root.left, leftMax, leftMin) && isValidBSTHelper(root.right, rightMax, rightMin)
+  }
 
 }
