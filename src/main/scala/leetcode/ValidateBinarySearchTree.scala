821eefca9aca1f43ffd2779a596989d1a5f371e4
Žan Žibert <zzibert@plume.com>
2023-08-16 18:05:23 +0200

    Validate Binary Search Tree - much faster #98

--- src/main/scala/leetcode/ValidateBinarySearchTree.scala
+++ src/main/scala/leetcode/ValidateBinarySearchTree.scala
@@ -1,5 +1,7 @@
 package leetcode
 
+import scala.collection.mutable
+
 class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
   var value: Int = _value
   var left: TreeNode = _left
@@ -8,17 +10,60 @@ class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null)
 
 object ValidateBinarySearchTree {
   def isValidBST(root: TreeNode): Boolean = {
-    val values = isValidBSTHelper(root)
 
-    values == values.sorted.distinct
+    val seen = mutable.Map[Int, Boolean]()
+    isValidBSTHelper(root,maxValue = Int.MaxValue, minValue = Int.MinValue, seen)
   }
 
-  def isValidBSTHelper(root: TreeNode): List[Int] = {
+  def isValidBSTHelper(root: TreeNode, maxValue: Int, minValue: Int, seen: mutable.Map[Int, Boolean]): Boolean = {
     if (root == null) {
-      Nil
-    } else {
-      isValidBSTHelper(root.left) ::: List(root.value) ::: isValidBSTHelper(root.right)
+      return true
     }
+
+    if (root.value < minValue || root.value > maxValue || seen.getOrElseUpdate(root.value, false)) {
+      return false
+    }
+
+    seen += (root.value -> true)
+
+    val leftMin = {
+      if (root.value == Int.MinValue) {
+        Int.MinValue
+      } else if (root.value < minValue) {
+        root.value - 1
+      } else {
+        minValue
+      }
+    }
+
+    val leftMax =
+      if (root.value == Int.MinValue) {
+        Int.MinValue
+      } else if (root.value < maxValue) {
+        root.value - 1
+      } else {
+        maxValue
+      }
+
+    val rightMin =
+      if (root.value == Int.MaxValue) {
+        Int.MaxValue
+      } else if (root.value > minValue) {
+        root.value + 1
+      } else {
+        minValue
+      }
+
+    val rightMax =
+      if (root.value == Int.MaxValue) {
+        Int.MaxValue
+      } else if (root.value > maxValue) {
+        root.value + 1
+      } else {
+        maxValue
+      }
+
+    isValidBSTHelper(root.left, leftMax, leftMin, seen) && isValidBSTHelper(root.right, rightMax, rightMin, seen)
   }
 
 }
