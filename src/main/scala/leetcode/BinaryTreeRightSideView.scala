56cae5149e574b06a799ac5eccbfed10f75eb229
Žan Žibert <zzibert@plume.com>
2023-08-18 00:10:01 +0200

    Binary Tree Right Side View #199

--- src/main/scala/leetcode/BinaryTreeRightSideView.scala
+++ src/main/scala/leetcode/BinaryTreeRightSideView.scala
@@ -1,3 +1,38 @@
-package leetcode class BinaryTreeRightSideView {
+package leetcode
 
+import scala.collection.mutable
+class BinaryTreeRightSideView {
+  def rightSideView(root: TreeNode): List[Int] = {
+    if (root == null) {
+      return Nil
+    }
+
+    val result = mutable.ListBuffer[Int]()
+    val queue = mutable.Queue[TreeNode]()
+
+    queue.addOne(root)
+
+    while (true) {
+      val row = queue.reverse.toList
+      if (row.isEmpty) {
+        return result.toList
+      } else {
+        result.addOne(row(0).value)
+      }
+      queue.clear()
+      row
+        .view
+        .reverse
+        .foreach { node =>
+          if (node.left != null) {
+            queue.addOne(node.left)
+          }
+          if (node.right != null) {
+            queue.addOne(node.right)
+          }
+        }
+    }
+
+    result.toList
+  }
 }
