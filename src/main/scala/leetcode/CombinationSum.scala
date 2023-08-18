b09735316821f4bf1c3f1f267e45c05e0a96817f
Žan Žibert <zzibert@plume.com>
2023-08-18 21:46:53 +0200

    Permutations #46

--- src/main/scala/leetcode/CombinationSum.scala
+++ src/main/scala/leetcode/CombinationSum.scala
@@ -1,11 +1,12 @@
 package leetcode
 
 import scala.collection.mutable
+import scala.collection.mutable.Set
 import scala.collection.mutable.ListBuffer
 
 object CombinationSum {
   def combinationSum(candidates: Array[Int], target: Int): List[List[Int]] = {
-    val combinations = mutable.Map[Int, ListBuffer[List[Int]]]()
+    val combinations = mutable.Map[Int, Set[List[Int]]]()
 
     candidates foreach { candidate =>
       val combination = ListBuffer[Int]()
@@ -13,23 +14,23 @@ object CombinationSum {
       while (combinationSum <= target) {
         combination.addOne(candidate)
         combinationSum = combination.sum
-        val buffer = combinations.getOrElseUpdate(combinationSum, ListBuffer[List[Int]]())
+        val buffer = combinations.getOrElseUpdate(combinationSum, Set[List[Int]]())
         buffer.addOne(combination.toList)
 
         val subtraction = target - combinationSum
         for {
           i <- 2 to subtraction
         } {
-          val buffer2 = combinations.getOrElseUpdate(i, ListBuffer[List[Int]]())
+          val buffer2 = combinations.getOrElseUpdate(i, Set[List[Int]]())
           buffer2 foreach { combination2 =>
             val newCombination = combination ++ combination2
-            val buffer3 = combinations.getOrElseUpdate(newCombination.sum, ListBuffer[List[Int]]())
-            buffer3.addOne(newCombination.toList)
+            val buffer3 = combinations.getOrElseUpdate(newCombination.sum, Set[List[Int]]())
+            buffer3.addOne(newCombination.toList.sorted)
           }
         }
       }
     }
 
-    combinations.getOrElse(target, ListBuffer[List[Int]]()).distinct.toList
+    combinations.getOrElse(target, ListBuffer[List[Int]]()).toList
   }
 }
