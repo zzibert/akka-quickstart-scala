97f4853cd00c600340e2b7d076de01261107f22f
Žan Žibert <zzibert@plume.com>
2023-08-20 11:34:53 +0200

    MinStack #155

--- src/main/scala/leetcode/MinStack.scala
+++ src/main/scala/leetcode/MinStack.scala
@@ -1,3 +1,35 @@
-package leetcode object MinStack {
+package leetcode
+
+import scala.collection.mutable
+
+class MinStack() {
+  val stack = mutable.Stack[(Int, Int)]()
+  var minimum = Int.MaxValue
+
+  def push(`val`: Int) {
+    if (`val` < minimum) {
+      minimum = `val`
+    }
+
+    stack.push((`val`, minimum))
+  }
+
+  def pop() {
+    stack.pop()
+    if (stack.nonEmpty) {
+      val (_, newMin) = stack.top
+      minimum = newMin
+    } else {
+      minimum = Int.MaxValue
+    }
+  }
+
+  def top(): Int = {
+    stack.top._1
+  }
+
+  def getMin(): Int = {
+    stack.top._2
+  }
 
 }
