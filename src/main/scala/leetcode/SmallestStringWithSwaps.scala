60a822e4007945c8af805984fa7c2a70e8f18baf
Žan Žibert <zzibert@plume.com>
2023-09-04 21:09:09 +0200

    Smallest String With Swaps - solves 35 / 36 test cases

---
+++ src/main/scala/leetcode/SmallestStringWithSwaps.scala
@@ -0,0 +1,52 @@
+package leetcode
+
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+
+object SmallestStringWithSwaps {
+  def smallestStringWithSwaps(s: String, pairs: List[List[Int]]): String = {
+    val length = s.length
+    val roots = ArrayBuffer[Int]()
+    val result: Array[Char] = Array.fill(length)(' ')
+
+    for (i <- 0 until length) {
+      roots.addOne(i)
+    }
+
+    pairs foreach { pair =>
+      val node0 = pair(0)
+      val node1 = pair(1)
+
+      if (roots(node0) != roots(node1)) {
+        val newRoot = roots(node0)
+        val oldRoot = roots(node1)
+
+        for (i <- 0 until length) {
+          if (roots(i) == oldRoot) {
+            roots(i) = newRoot
+          }
+        }
+      }
+    }
+
+    val groups = mutable.Map[Int, ArrayBuffer[Int]]()
+
+    roots.zipWithIndex foreach {
+      case (group, index) =>
+        val buffer = groups.getOrElseUpdate(group, ArrayBuffer[Int]())
+        buffer.addOne(index)
+    }
+
+    groups.values foreach { indices =>
+      val characters = indices.map(c => s(c)).sorted
+      val sortedIndices = indices.sorted
+
+      sortedIndices.toSeq.zip(characters.toSeq) foreach {
+        case (i, c) =>
+          result(i) = c
+      }
+    }
+
+    String.valueOf(result)
+  }
+}
