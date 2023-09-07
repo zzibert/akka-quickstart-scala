b9f1d07d26c48a946ded79d51f4a5c3947e356d8
Žan Žibert <zzibert@plume.com>
2023-09-07 18:18:53 +0200

    Find if Path Exists in Graph #1971

--- src/main/scala/leetcode/FindIfPathExistsInGraph.scala
+++ src/main/scala/leetcode/FindIfPathExistsInGraph.scala
@@ -1,6 +1,7 @@
 package leetcode
 
 import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 
 object FindIfPathExistsInGraph {
   def validPath(n: Int, edges: Array[Array[Int]], source: Int, destination: Int): Boolean = {
@@ -8,28 +9,34 @@ object FindIfPathExistsInGraph {
       return true
     }
 
-    val roots = mutable.ArrayBuffer[Int]()
-
-    for (i <- 0 until n) {
-      roots.addOne(i)
-    }
+    val paths = mutable.Map[Int, ArrayBuffer[Int]]()
+    val visited = mutable.Map[Int, Boolean]()
 
     edges foreach { edge =>
-      val root1 = roots(edge(0))
-      val root2 = roots(edge(1))
+      val buffer1 = paths.getOrElseUpdate(edge(0), ArrayBuffer[Int]())
+      buffer1.addOne(edge(1))
 
-      if (root1 != root2) {
-        val newRoot = root1
-        val replaceRoot = root2
-
-        for (i <- 0 until n) {
-          if (roots(i) == replaceRoot) {
-            roots(i) = newRoot
-          }
-        }
-      }
+      val buffer2 = paths.getOrElseUpdate(edge(1), ArrayBuffer[Int]())
+      buffer2.addOne(edge(0))
     }
 
-    roots(source) == roots(destination)
+    pathFinder(source, destination, paths, visited)
+  }
+
+  def pathFinder(node: Int, destination: Int, paths: mutable.Map[Int, ArrayBuffer[Int]], visited: mutable.Map[Int, Boolean]): Boolean = {
+    if (node == destination) {
+      true
+    } else {
+      visited += (node -> true)
+      val potentials = paths.getOrElse(node, ArrayBuffer.empty).filterNot(visited.getOrElse(_, false))
+
+      potentials foreach { potential =>
+        if (pathFinder(potential, destination, paths, visited)) {
+          return true
+        }
+      }
+
+      false
+    }
   }
 }
