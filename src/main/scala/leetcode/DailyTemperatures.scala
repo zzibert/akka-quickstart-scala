568df81533a8c188ca540adcf3b4823c292e02e3
Žan Žibert <zzibert@plume.com>
2023-09-30 18:03:12 +0200

    Daily Temperatures - suboptimal solution

--- src/main/scala/leetcode/DailyTemperatures.scala
+++ src/main/scala/leetcode/DailyTemperatures.scala
@@ -1,3 +1,27 @@
-package leetcode object DailyTemperatures {
+package leetcode
+object DailyTemperatures {
+  def dailyTemperatures(temperatures: Array[Int]): Array[Int] = {
+    val result = Array.fill(temperatures.length)(0)
 
+    for (i <- 0 until temperatures.length) {
+      result(i) = traverseUntillBigger(i, temperatures)
+    }
+
+    result
+  }
+
+  def traverseUntillBigger(index: Int, nums: Array[Int]): Int = {
+    val number = nums(index)
+    var result = 0
+
+    for (i <- index+1 until nums.length) {
+      if (nums(i) > number) {
+        return result
+      } else {
+        result += 1
+      }
+    }
+
+    0
+  }
 }
