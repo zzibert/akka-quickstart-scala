c6dab71aed661c9dedacb0f6e59183ee14700952
Žan Žibert <zzibert@plume.com>
2023-09-30 18:42:24 +0200

    Daily Temperatures #739

--- src/main/scala/leetcode/DailyTemperatures.scala
+++ src/main/scala/leetcode/DailyTemperatures.scala
@@ -1,27 +1,26 @@
 package leetcode
+
+import scala.collection.mutable
+
 object DailyTemperatures {
   def dailyTemperatures(temperatures: Array[Int]): Array[Int] = {
+    val stack = mutable.Stack[(Int, Int)]()
     val result = Array.fill(temperatures.length)(0)
 
     for (i <- 0 until temperatures.length) {
-      result(i) = traverseUntillBigger(i, temperatures)
+      val current = (i, temperatures(i))
+      while (stack.nonEmpty && stack.head._2 < current._2) {
+        val head = stack.pop()
+        result(head._1) = current._1 - head._1
+      }
+      stack.push(current)
+    }
+
+    while (stack.nonEmpty) {
+      val current = stack.pop()
+      result(current._1) = 0
     }
 
     result
-  }
-
-  def traverseUntillBigger(index: Int, nums: Array[Int]): Int = {
-    val number = nums(index)
-    var result = 0
-
-    for (i <- index+1 until nums.length) {
-      if (nums(i) > number) {
-        return result
-      } else {
-        result += 1
-      }
-    }
-
-    0
   }
 }
