2af209772d379a6a8148c579f546230fe55bd4d4
Žan Žibert <zzibert@plume.com>
2023-10-21 11:50:54 +0200

    Trapping Rain Water #42

---
+++ src/main/scala/leetcode/TrappingRainWater.scala
@@ -0,0 +1,49 @@
+package leetcode
+
+class TrappingRainWater {
+  def trap(height: Array[Int]): Int = {
+    val length = height.length
+    var result = 0
+    var left = 0
+
+    while (left < length) {
+      if (height(left) > 0) {
+        val right = findRight(left, height)
+        if (right != -1) {
+          result += calculateWater(left, right, height)
+          left = right
+        } else {
+          height(left) -= 1
+        }
+      } else {
+        left += 1
+      }
+    }
+
+    result
+  }
+
+  def findRight(left: Int, heights: Array[Int]): Int = {
+    val leftHeight = heights(left)
+    for (i <- left+1 until heights.length) {
+      if (leftHeight <= heights(i)) {
+        return i
+      }
+    }
+
+    -1
+  }
+
+  def calculateWater(left: Int, right: Int, heights: Array[Int]): Int = {
+    var result = 0
+    val height = Math.min(heights(left), heights(right))
+
+    for (i <- left+1 to right-1) {
+      result += height - heights(i)
+    }
+
+    result
+  }
+
+
+}
