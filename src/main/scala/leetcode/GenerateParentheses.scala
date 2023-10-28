3fa8caf39d30c5aaac700c6c149d472238ba748b
Žan Žibert <zzibert@plume.com>
2023-10-28 11:02:58 +0200

    Generate Parentheses #22 - naive attempt

--- src/main/scala/leetcode/GenerateParentheses.scala
+++ src/main/scala/leetcode/GenerateParentheses.scala
@@ -1,3 +1,29 @@
-//package leetcode object GenerateParentheses {
-//
-//}
+package leetcode
+
+import scala.collection.mutable
+
+object GenerateParentheses {
+  def generateParenthesis(n: Int): List[String] = {
+    val queue = mutable.Queue[String]()
+    var counter = 1
+
+    if (n < 1) {
+      Nil
+    } else {
+      queue.enqueue("()")
+      while (counter < n) {
+        val length = queue.length
+        for (_ <- 0 until length) {
+          val parentheses = queue.dequeue()
+          for (i <- 0 until parentheses.length) {
+            val newValue = parentheses.take(i) + "()" + parentheses.drop(i)
+            queue.enqueue(newValue)
+          }
+        }
+        counter += 1
+      }
+
+      queue.toList.distinct
+    }
+  }
+}
