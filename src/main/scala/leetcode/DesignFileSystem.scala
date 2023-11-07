6804f80d5f12aaf5678ca03c4189a8d4c66bcb8d
Žan Žibert <zzibert@plume.com>
2023-11-07 20:18:53 +0100

    Design File System #1166 - Implemented with Trie

--- src/main/scala/leetcode/DesignFileSystem.scala
+++ src/main/scala/leetcode/DesignFileSystem.scala
@@ -4,28 +4,60 @@ import scala.collection.mutable
 
 object DesignFileSystem {
   class FileSystem() {
-    val directory = mutable.Map[String, Int]()
-    directory.addOne("", -1)
+
+    val zeroNode = new PathNode("", -1)
+
+    class PathNode(_path: String, _value: Int) {
+      val children = mutable.ArrayBuffer[PathNode]()
+
+      def path: String = _path
+
+      def value: Int = _value
+
+      def insert(path: Array[String], value: Int): Boolean = {
+        if (path.length == 1) {
+          if (!children.exists(_.path == path.head)) {
+            val newNode = new PathNode(path.head, value)
+            children.addOne(newNode)
+            true
+          } else {
+            false
+          }
+        } else {
+          val head = path.head
+          children.find(_.path == head) match {
+            case Some(next) =>
+              next.insert(path.tail, value)
+            case None =>
+              false
+          }
+        }
+      }
+
+      def find(path: Array[String]): Int = {
+        if (path.length == 0) {
+          _value
+        } else {
+          val head = path.head
+          children.find(_.path == head) match {
+            case Some(next) =>
+              next.find(path.tail)
+            case None =>
+              -1
+          }
+        }
+      }
+
+    }
 
     def createPath(path: String, value: Int): Boolean = {
-      if (directory.isDefinedAt(path)) {
-        false
-      } else if (isParentPresent(path)) {
-        directory.addOne(path, value)
-        true
-      } else {
-        false
-      }
+      val chunks = path.split('/').tail
+      zeroNode.insert(chunks, value)
     }
 
     def get(path: String): Int = {
-      directory.getOrElse(path, -1)
-    }
-
-    def isParentPresent(path: String): Boolean = {
-      val chunks = path.split("/")
-      val parent = chunks.take(chunks.length - 1).mkString("/")
-      directory.isDefinedAt(parent)
+      val chunks = path.split('/').tail
+      zeroNode.find(chunks)
     }
 
   }
