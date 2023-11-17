cbb78294b418f83e144dd26af480d85f132d11b7
Žan Žibert <zzibert@plume.com>
2023-11-17 14:01:51 +0100

    Serialize and Deserialize Binary tree - 2nd attempt

--- src/main/scala/leetcode/SerializeAndDeserializeBinaryTree.scala
+++ src/main/scala/leetcode/SerializeAndDeserializeBinaryTree.scala
@@ -3,64 +3,61 @@ package leetcode
 import scala.collection.mutable
 
 class Codec {
-  private val keyValue = mutable.Map[Int, Int]()
+  var nodeString = ""
+  var chunks: List[Array[String]] = Nil
 
   // Encodes a list of strings to a single string.
   def serialize(root: TreeNode): String = {
-    traverse(root, 0)
-    val result = keyValue.view.toList.map(node => s"${node._1}+${node._2}").mkString("|")
-    keyValue.clear()
-    result
+    serializeHelper(root, 0)
+
+    nodeString
+  }
+
+  def leftChildIndex(parentIndex: Int): Int = {
+    (parentIndex * 2) + 1
+  }
+
+  def rightChildIndex(parentIndex: Int): Int = {
+    (parentIndex * 2) + 2
+  }
+
+  def serializeHelper(root: TreeNode, position: Int): Unit = {
+    if (root != null) {
+      nodeString += s"|${position}+${root.value}"
+
+      serializeHelper(root.left, leftChildIndex(position))
+      serializeHelper(root.right, rightChildIndex(position))
+    }
   }
 
   // Decodes a single string to a list of strings.
   def deserialize(data: String): TreeNode = {
-    if (data.nonEmpty) {
-      val nodes = data.split('|')
-      nodes foreach { node =>
-        val tuple = node.split('+')
-        val key = tuple.head.toInt
-        val value = tuple.last.toInt
-        keyValue.update(key, value)
-      }
+    chunks =
+      data
+        .split('|')
+        .view
+        .tail
+        .map(pair => pair.split('+'))
+        .toList
 
-      deTraverse(0)
-    } else {
-      null
-    }
+
+    deserializeHelper(0)
   }
 
-  def deTraverse(index: Int): TreeNode = {
-    keyValue.get(index) match {
-      case Some(value) =>
-        val node = new TreeNode(value)
-        node.left = deTraverse(getLeftChildIndex(index))
-        node.right = deTraverse(getRightChildIndex(index))
+  def deserializeHelper(position: Int): TreeNode = {
+    val rootOption = chunks.find(pair => pair(0).toInt == position)
+
+    rootOption match {
+      case Some(root) =>
+        val node = new TreeNode(root(1).toInt)
+        node.left = deserializeHelper(leftChildIndex(position))
+        node.right = deserializeHelper(rightChildIndex(position))
         node
+
 
       case None =>
         null
-
     }
-  }
 
-  def getLeftChildIndex(parentIndex: Int): Int = {
-    (parentIndex * 2) + 1
-  }
-
-  def getRightChildIndex(parentIndex: Int): Int = {
-    (parentIndex * 2) + 2
-  }
-
-  def traverse(root: TreeNode, index: Int): Unit = {
-    if (root != null) {
-      keyValue.update(index, root.value)
-
-      val leftIndex = (index * 2) + 1
-      traverse(root.left, leftIndex)
-
-      val rightIndex = (index * 2) + 2
-      traverse(root.right, rightIndex)
-    }
   }
 }
