9a9009d33f14b1d0ed38e0a9107b696791aa8c00
Žan Žibert <zzibert@plume.com>
2023-12-12 21:50:02 +0100

    Top K Frequent Elements #347

--- src/main/scala/leetcode/TopKFrequentElements.scala
+++ src/main/scala/leetcode/TopKFrequentElements.scala
@@ -1,3 +1,27 @@
-package leetcode class TopKFrequentElements {
+package leetcode
 
+import scala.collection.mutable
+
+object TopKFrequentElements {
+
+  def topKFrequent(nums: Array[Int], k: Int): Array[Int] = {
+    val values = mutable.Map[Int, Int]()
+
+    nums foreach { number =>
+      values.get(number) match {
+        case Some(counter) =>
+          values.update(number, counter + 1)
+
+        case None =>
+          values.update(number, 1)
+      }
+    }
+
+    values.toArray.view
+      .sortBy(-_._2)
+      .take(k)
+      .map(_._1)
+      .toArray
+
+  }
 }
