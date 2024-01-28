ea0409c166338531aa0c554041bd000e0bf0ca5d
Žan Žibert <zzibert@plume.com>
2024-01-28 10:44:13 +0100

    Word Ladder #127

--- src/main/scala/leetcode/WordLadder.scala
+++ src/main/scala/leetcode/WordLadder.scala
@@ -1,78 +1,71 @@
 package leetcode
 
+import scala.collection.mutable
+
 object WordLadder {
-  def ladderLength(beginWord: String, endWord: String, wordList: List[String]): Int = {
-    val oneLetterDifference = Map[String, Set[String]]()
-    val tried = Map[String, Boolean]()
+  def ladderLength(beginWord: String,
+                   endWord: String,
+                   wordList: List[String]): Int = {
+    val differByOneLetter = mutable.Map[String, mutable.ListBuffer[String]]()
+    val seen = mutable.Map[String, Boolean]()
+    val queue = mutable.Queue[(String, Int)]()
 
-    for (i <- 0 until wordList.length) {
-      val word = wordList(i)
-      var buffer = oneLetterDifference.getOrElse(word, Set[String]())
+    val completeList = wordList.appended(beginWord).toArray
 
-      for (j <- i+1 until wordList.length) {
-        val secondWord = wordList(j)
-        if (differByOneLetter(word.toCharArray, secondWord.toCharArray)) {
-          buffer.addOne(secondWord)
-          val secondBuffer = oneLetterDifference.getOrElse(secondWord, mutable.Set[String]())
-          secondBuffer.addOne(word)
-          oneLetterDifference.update(secondWord, secondBuffer)
-        }
-      }
-
-      oneLetterDifference.update(word, buffer)
-    }
-
-    val buffer = mutable.Set[String]()
-    val beginWordArray = beginWord.toCharArray
-
-    for (i <- 0 until wordList.length) {
-      if (differByOneLetter(beginWordArray, wordList(i).toCharArray)) {
-        buffer.addOne(wordList(i))
+    for {
+      i <- 0 until completeList.length
+      j <- i + 1 until completeList.length
+    } {
+      if (isDifferenceByOneLetter(completeList(i), completeList(j))) {
+        val bufferI = differByOneLetter.getOrElseUpdate(
+          completeList(i),
+          mutable.ListBuffer[String]())
+        bufferI.addOne(completeList(j))
+        val bufferJ = differByOneLetter.getOrElseUpdate(
+          completeList(j),
+          mutable.ListBuffer[String]())
+        bufferJ.addOne(completeList(i))
       }
     }
 
-    oneLetterDifference.update(beginWord, buffer)
-
-    val queue = mutable.Queue[(Int, String)]()
-
-    queue.enqueue((1, beginWord))
-    tried.update(beginWord, true)
+    queue.enqueue((beginWord, 1))
 
     while (queue.nonEmpty) {
-      val (length, word) = queue.dequeue()
-      println(s"length: $length, word: $word")
+      val (word, counter) = queue.dequeue()
       if (word == endWord) {
-        return length
+        return counter
       } else {
-        oneLetterDifference.get(word) match {
-          case Some(buffer) =>
-            buffer.view
-              .filterNot(word => tried.getOrElse(word, false))
-              .foreach { w =>
-                println(s"enqueining: length: ${length+1} word: $w")
-                tried.update(w, true)
-                queue.enqueue((length+1, w))
+        differByOneLetter.get(word) match {
+          case Some(neighbors) =>
+            neighbors foreach { neighbor =>
+              if (!seen.getOrElse(neighbor, false)) {
+                seen.update(neighbor, true)
+                queue.enqueue((neighbor, counter + 1))
               }
-          case None =>
+            }
+
+          case _ =>
         }
+
       }
     }
 
     0
   }
 
-  def differByOneLetter(word1: Array[Char], word2: Array[Char]): Boolean = {
-    var count = 0
+  def isDifferenceByOneLetter(word1: String, word2: String): Boolean = {
+    var differ = false
 
     for (i <- 0 until word1.length) {
       if (word1(i) != word2(i)) {
-        count += 1
-        if (count > 1) {
+        if (!differ) {
+          differ = true
+        } else {
           return false
         }
       }
     }
 
-    count < 2
+    differ
   }
 }
