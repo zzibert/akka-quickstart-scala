f288bd8d398de6f7332a65af49b557d73bea396e
Žan Žibert <zzibert@plume.com>
2024-02-17 18:58:07 +0100

    Minimum window substring #76

--- src/main/scala/leetcode/MinimumWindowSubstring.scala
+++ src/main/scala/leetcode/MinimumWindowSubstring.scala
@@ -1,54 +1,60 @@
 package leetcode
 
 import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
 
 object MinimumWindowSubstring {
   def minWindow(s: String, t: String): String = {
-    for (length <- t.length to s.length) {
-      val example = ArrayBuffer[Char]()
+    var length = t.length
 
-      for (i <- 0 until length) {
-        example.addOne(s(i))
+    while (length <= s.length) {
+      val characters = mutable.Map[Char, Int]()
+      t foreach { c =>
+        val count = characters.getOrElseUpdate(c, 0)
+        characters.update(c, count + 1)
       }
 
-      for (i <- length until s.length) {
-        if (isSubSequence(example, t)) {
-          return example.mkString("")
-        } else {
-          example.dropInPlace(1).addOne(s(i))
+      // initial fill
+      for (i <- 0 until length) {
+        val character = s(i)
+        characters.get(character) match {
+          case Some(count) =>
+            characters.update(character, count - 1)
+
+          case _ =>
         }
       }
-      if (isSubSequence(example, t)) {
 
-        return example.mkString("")
+      if (allEqualOrLessThanZero(characters)) {
+        return s.substring(0, length)
       }
+
+      for (next <- length until s.length) {
+        val delete = s(next-length)
+        characters.get(delete) match {
+          case Some(count) =>
+            characters.update(delete, count+1)
+          case _ =>
+        }
+
+        val add = s(next)
+        characters.get(add) match {
+          case Some(count) =>
+            characters.update(add, count-1)
+            if (count == 1 && allEqualOrLessThanZero(characters)) {
+              println(s"the length is $length")
+              return s.substring(next-(length-1), next+1)
+            }
+          case _ =>
+        }
+      }
+
+      length += 1
     }
+
     ""
   }
 
-  def isSubSequence(example: ArrayBuffer[Char], target: String): Boolean = {
-    val chars = mutable.Map[Char, Int]()
-
-    for (c <- example) {
-      val current = chars.getOrElse(c, 0)
-      chars += (c -> (current+1))
-    }
-
-    for (c <- target) {
-      chars.get(c) match {
-        case None =>
-          return false
-
-        case Some(value) =>
-          if (value < 1) {
-            return false
-          } else {
-            chars += (c -> (value-1))
-          }
-      }
-    }
-
-    true
+  def allEqualOrLessThanZero(characters: mutable.Map[Char, Int]): Boolean = {
+    characters.forall(_._2 < 1)
   }
 }
