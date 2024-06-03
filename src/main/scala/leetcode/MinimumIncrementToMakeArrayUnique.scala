b269df63ce927879b0407ce9013012a2cc334444
Žan Žibert <zzibert@plume.com>
2024-06-03 21:30:03 +0200

    Minimum Increment to Make Array Unique #945

--- src/main/scala/leetcode/MinimumIncrementToMakeArrayUnique.scala
+++ src/main/scala/leetcode/MinimumIncrementToMakeArrayUnique.scala
@@ -1,3 +1,29 @@
-package leetcode object MinimumIncrementToMakeArrayUnique {
+package leetcode
 
+import scala.collection.mutable
+
+object Solution {
+  def minIncrementForUnique(nums: Array[Int]): Int = {
+    val sortedNums = nums.sorted
+    var result = 0
+    var highest = 0
+    val used = mutable.Map[Int, Boolean]()
+
+    var i = 0
+
+    while (i < sortedNums.length) {
+      used.get(sortedNums(i)) match {
+        case Some(_) =>
+          result += (highest + 1) - sortedNums(i)
+          sortedNums(i) = (highest + 1)
+
+        case None =>
+          used += (sortedNums(i) -> true)
+          highest = sortedNums(i)
+          i += 1
+      }
+    }
+
+    result
+  }
 }
