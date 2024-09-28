cc92e90690ce4e5d6a6605a757bc54fa4113f9c6
Žan Žibert <zzibert@plume.com>
2024-09-28 09:24:16 +0200

    remove insert interval

--- src/main/scala/leetcode/InsertInterval.scala
+++ src/main/scala/leetcode/InsertInterval.scala
@@ -2,41 +2,8 @@ package leetcode
 
 import scala.collection.mutable
 
-object InsertInterval {
+object Solution {
   def insert(intervals: Array[Array[Int]], newInterval: Array[Int]): Array[Array[Int]] = {
-    val insertResult = mutable.ArrayBuffer[Array[Int]]()
-    var insertOption: Option[Array[Int]] = Some(newInterval)
-    // insert Stage
-    intervals foreach { interval =>
-      if (insertOption.exists(_(0) < interval(0))) {
-        insertOption foreach { insert =>
-          insertResult.addOne(insert)
-          insertOption = None
-        }
-      }
-      insertResult.addOne(interval)
-    }
 
-    insertOption foreach { insert =>
-      insertResult.addOne(insert)
-    }
-
-    merge(insertResult)
-
-    insertResult.toArray
-  }
-
-  def merge(intervals: mutable.ArrayBuffer[Array[Int]]): Unit = {
-    var i = 0
-    while (i < (intervals.length - 1)) {
-      if (intervals(i)(1) > intervals(i+1)(1)) {
-        intervals.remove(i+1)
-      } else if (intervals(i)(1) >= intervals(i + 1)(0)) {
-        intervals(i)(1) = intervals(i+1)(1)
-        intervals.remove(i+1)
-      } else {
-        i += 1
-      }
-    }
   }
 }
