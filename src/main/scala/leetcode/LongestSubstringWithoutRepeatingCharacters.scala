14f497efdab4e85f2b9294fb75444ad57d145c3b
Žan Žibert <zzibert@plume.com>
2024-09-28 11:25:26 +0200

    Reset Longest Substring Without Repeating Characters

--- src/main/scala/leetcode/LongestSubstringWithoutRepeatingCharacters.scala
+++ src/main/scala/leetcode/LongestSubstringWithoutRepeatingCharacters.scala
@@ -1,25 +1,8 @@
 package leetcode
 
-import scala.collection.mutable
 
-object LongestSubstringWithoutRepeatingCharacters {
+object Solution {
   def lengthOfLongestSubstring(s: String): Int = {
-    val seen = mutable.Map[Char, Boolean]()
-    var result = 0
 
-    for (i <- 0 until s.length) {
-      seen.clear()
-      var localResult = 0
-      var start = i
-      while (start < s.length && seen.get(s(start)).isEmpty) {
-        seen += (s(start) -> true)
-        localResult += 1
-        start += 1
-      }
-      if (localResult > result) {
-        result = localResult
-      }
-    }
-    result
   }
 }
