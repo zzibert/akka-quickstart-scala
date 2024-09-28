4ae5601089641b720b85d64796717ddc733a1d77
Žan Žibert <zzibert@plume.com>
2024-09-28 16:19:14 +0200

    Reset Evaluate Reverse Polish Notation

--- src/main/scala/leetcode/EvaluateReversePolishNotation.scala
+++ src/main/scala/leetcode/EvaluateReversePolishNotation.scala
@@ -2,32 +2,8 @@ package leetcode
 
 import scala.collection.mutable
 
-object EvaluateReversePolishNotation {
+object Solution {
   def evalRPN(tokens: Array[String]): Int = {
-    val symbols = List("+", "-", "*", "/")
-    val stack = mutable.Stack[Int]()
 
-    tokens foreach { token =>
-      if (symbols.contains(token)) {
-        val operand2 = stack.pop()
-        val operand1 = stack.pop()
-        var result = 0
-        token match {
-          case "+" =>
-            result = operand1 + operand2
-          case "-" =>
-            result = operand1 - operand2
-          case "*" =>
-            result = operand1 * operand2
-          case "/" =>
-            result = operand1 / operand2
-        }
-        stack.push(result)
-      } else {
-        stack.push(token.toInt)
-      }
-    }
-
-    stack.pop()
   }
 }
