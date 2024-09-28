dce2ddd0956303e9b9a3378cb2b44ce11fa39303
Žan Žibert <zzibert@plume.com>
2024-09-28 15:42:50 +0200

    Binary Tree Level Order Traversal  #102 Redone

--- src/main/scala/leetcode/ThreeSum.scala
+++ src/main/scala/leetcode/ThreeSum.scala
@@ -1,3 +1,22 @@
-//package leetcode class ThreeSum {
-//
-//}
+
+
+object Solution {
+  def threeSum(nums: Array[Int]): List[List[Int]] = {
+
+    val length = nums.length
+    var result = Set[List[Int]]()
+    (for {
+      i <- 0 until length
+      j <- i + 1 until length
+      k <- j + 1 until length
+      if (nums(i) + nums(j) + nums(k)) == 0
+    } yield {
+      val list = List(nums(i), nums(j), nums(k)).sorted
+      if (!result.contains(list)) {
+        result += list
+      }
+    }).toList
+
+    result.toList
+  }
+}
