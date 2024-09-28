aebfad75b5cfbd8e63c0daa6669b85ae154a02e8
Žan Žibert <zzibert@plume.com>
2024-09-28 11:03:12 +0200

    Reset K closest points to origin

--- src/main/scala/leetcode/KClosestPointsToOrigin.scala
+++ src/main/scala/leetcode/KClosestPointsToOrigin.scala
@@ -1,15 +1,7 @@
 package leetcode
 
-object KClosestPointsToOrigin {
+object Solution {
   def kClosest(points: Array[Array[Int]], k: Int): Array[Array[Int]] = {
 
-    implicit val kClosestOrdering: Ordering[Array[Int]] = Ordering.by { array =>
-      val sumOfSquares = array.map(Math.pow(_, 2)).sum
-
-      sumOfSquares
-    }
-    points.sortInPlace()
-
-    points.take(k)
   }
 }
