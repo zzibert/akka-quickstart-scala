d3e4963f1e1703bbe98d46844caae87e0acba910
Žan Žibert <zzibert@plume.com>
2024-09-28 16:18:17 +0200

    Clone Graph #133 Redone

--- src/main/scala/leetcode/CloneGraph.scala
+++ src/main/scala/leetcode/CloneGraph.scala
@@ -1,16 +1,32 @@
 package leetcode
 
+import scala.collection.mutable
 
-/**
- * Definition for a Node.
- * class Node(var _value: Int) {
- *   var value: Int = _value
- *   var neighbors: List[Node] = List()
- * }
- */
+class Node(var _value: Int) {
+  var value: Int = _value
+  var neighbors: List[Node] = List()
+}
 
 object Solution {
   def cloneGraph(graph: Node): Node = {
+    val nodes = mutable.Map[Int, Node]()
 
+    if (graph != null) {
+      cloneGraphHelper(graph, nodes)
+    } else {
+      null
+    }
+  }
+
+  def cloneGraphHelper(node: Node, nodes: mutable.Map[Int, Node]): Node = {
+    nodes.get(node.value) match {
+      case Some(found) => found
+
+      case None =>
+        val newNode = new Node(node.value)
+        nodes.update(node.value, newNode)
+        newNode.neighbors = node.neighbors.map(cloneGraphHelper(_, nodes))
+        newNode
+    }
   }
 }
