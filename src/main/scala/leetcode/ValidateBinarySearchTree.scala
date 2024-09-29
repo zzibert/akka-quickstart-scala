6ee9a7a5e357e80cfa13b5195d9f00e39dfe0333
Žan Žibert <zzibert@plume.com>
2024-09-29 14:28:15 +0200

    Redo Validate Binary Search Tree #98

--- src/main/scala/leetcode/ValidateBinarySearchTree.scala
+++ src/main/scala/leetcode/ValidateBinarySearchTree.scala
@@ -1,69 +1,27 @@
-//package leetcode
-//
-//import scala.collection.mutable
-//
-//class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
-//  var value: Int = _value
-//  var left: TreeNode = _left
-//  var right: TreeNode = _right
-//}
-//
-//object ValidateBinarySearchTree {
-//  def isValidBST(root: TreeNode): Boolean = {
-//
-//    val seen = mutable.Map[Int, Boolean]()
-//    isValidBSTHelper(root,maxValue = Int.MaxValue, minValue = Int.MinValue, seen)
-//  }
-//
-//  def isValidBSTHelper(root: TreeNode, maxValue: Int, minValue: Int, seen: mutable.Map[Int, Boolean]): Boolean = {
-//    if (root == null) {
-//      return true
-//    }
-//
-//    if (root.value < minValue || root.value > maxValue || seen.getOrElseUpdate(root.value, false)) {
-//      return false
-//    }
-//
-//    seen += (root.value -> true)
-//
-//    val leftMin = {
-//      if (root.value == Int.MinValue) {
-//        Int.MinValue
-//      } else if (root.value < minValue) {
-//        root.value - 1
-//      } else {
-//        minValue
-//      }
-//    }
-//
-//    val leftMax =
-//      if (root.value == Int.MinValue) {
-//        Int.MinValue
-//      } else if (root.value < maxValue) {
-//        root.value - 1
-//      } else {
-//        maxValue
-//      }
-//
-//    val rightMin =
-//      if (root.value == Int.MaxValue) {
-//        Int.MaxValue
-//      } else if (root.value > minValue) {
-//        root.value + 1
-//      } else {
-//        minValue
-//      }
-//
-//    val rightMax =
-//      if (root.value == Int.MaxValue) {
-//        Int.MaxValue
-//      } else if (root.value > maxValue) {
-//        root.value + 1
-//      } else {
-//        maxValue
-//      }
-//
-//    isValidBSTHelper(root.left, leftMax, leftMin, seen) && isValidBSTHelper(root.right, rightMax, rightMin, seen)
-//  }
-//
-//}
+package leetcode
+
+object Solution {
+  def isValidBST(root: TreeNode): Boolean = {
+
+    isValidBSTHelper(root, None, None)
+  }
+
+  def isValidBSTHelper(
+      root: TreeNode,
+     minOption: Option[Int],
+     maxOption: Option[Int]
+                      ): Boolean = {
+    if (root == null) {
+      true
+    } else {
+      if ((minOption.nonEmpty && root.value <= minOption.get) || (maxOption.nonEmpty && root.value >= maxOption.get)) {
+        false
+      } else {
+        isValidBSTHelper(root.left, minOption, Some(root.value)) && isValidBSTHelper(
+          root.right,
+          Some(root.value),
+          maxOption)
+      }
+    }
+  }
+}
