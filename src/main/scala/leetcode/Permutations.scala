453473f379dfe130a7b6881d9bfae053c94982f2
Žan Žibert <zzibert@plume.com>
2024-09-30 10:47:41 +0200

    reset permutations

--- src/main/scala/leetcode/Permutations.scala
+++ src/main/scala/leetcode/Permutations.scala
@@ -2,23 +2,8 @@ package leetcode
 
 import scala.collection.mutable
 
-object Permutations {
+object Solution {
   def permute(nums: Array[Int]): List[List[Int]] = {
-    val result = mutable.ListBuffer[List[Int]]()
 
-    permutateHelper(Nil, nums, result)
-
-    result.toList
-  }
-
-  def permutateHelper(permutation: List[Int], nums: Array[Int], result: mutable.ListBuffer[List[Int]]): Unit = {
-    if (nums.length > 0) {
-      nums.zipWithIndex foreach {
-        case (num, index) =>
-          permutateHelper(num :: permutation, nums.patch(index, Nil, 1), result)
-      }
-    } else {
-      result.addOne(permutation)
-    }
   }
 }
