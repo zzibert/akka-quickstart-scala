7a6107277e984ea0f745483a703613d71a865de6
Žan Žibert <zzibert@plume.com>
2024-10-05 21:16:21 +0200

    Lowest Common Ancestor of A Binary Tree #236 Redone

--- src/main/scala/leetcode/LowestCommonAncestorOfABinaryTreeIII.scala
+++ src/main/scala/leetcode/LowestCommonAncestorOfABinaryTreeIII.scala
@@ -1,7 +1,36 @@
 package leetcode
 
-object Solution {
-  def lowestCommonAncestor(root: TreeNode, p: TreeNode, q: TreeNode): TreeNode = {
+import scala.collection.mutable
 
+object Solution {
+  def lowestCommonAncestor(root: TreeNode,
+                           p: TreeNode,
+                           q: TreeNode): TreeNode = {
+    val pPathElement = mutable.ListBuffer[Array[TreeNode]]()
+    val qPathElement = mutable.ListBuffer[Array[TreeNode]]()
+
+    findPath(root, p.value, Array.empty, pPathElement)
+    findPath(root, q.value, Array.empty, qPathElement)
+
+    val pPath = pPathElement.head.reverse
+    val qPath = qPathElement.head.reverse
+
+    qPath.find(element => pPath.contains(element)).getOrElse(new TreeNode())
+  }
+
+  def findPath(
+      root: TreeNode,
+      value: Int,
+      path: Array[TreeNode],
+      result: mutable.ListBuffer[Array[TreeNode]]
+  ): Unit = {
+    if (root != null) {
+      if (root.value == value) {
+        result.addOne(path :+ root)
+      } else {
+        findPath(root.left, value, path :+ root, result)
+        findPath(root.right, value, path :+ root, result)
+      }
+    }
   }
 }
