bdfae2a3cd4b5abed5e5313a3a72e5c8f8dc32d3
Žan Žibert <zzibert@plume.com>
2024-10-05 20:03:03 +0200

    Product of Array Except Self #238 Redone

--- src/main/scala/leetcode/ProductOfArrayExceptSelf.scala
+++ src/main/scala/leetcode/ProductOfArrayExceptSelf.scala
@@ -1,8 +1,37 @@
 package leetcode
 
+import scala.collection.mutable
 
 object Solution {
   def productExceptSelf(nums: Array[Int]): Array[Int] = {
+    val leftSide = mutable.Map[Int, Int]()
+    val rightSide = mutable.Map[Int, Int]()
+    val result: Array[Int] = Array.ofDim(nums.length)
 
+    var value = nums(0)
+    leftSide.update(0, value)
+
+    for (i <- 1 until nums.length) {
+      value *= nums(i)
+      leftSide.update(i, value)
+    }
+
+    value = 1
+    var i = nums.length - 1
+
+    while (i >= 0) {
+      value *= nums(i)
+      rightSide(i) = value
+      i -= 1
+    }
+
+    for (i <- 0 until nums.length) {
+      val leftAnswer = if (i == 0) 1 else leftSide(i - 1)
+      val rightAnswer = if (i < nums.length - 1) rightSide(i + 1) else 1
+
+      result(i) = leftAnswer * rightAnswer
+    }
+
+    result
   }
 }
