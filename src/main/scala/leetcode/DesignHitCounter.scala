d7b32315aa604550bf3ac44483aec0f11396306f
Žan Žibert <zzibert@plume.com>
2024-10-06 09:59:18 +0200

    Design Hit Counter Reset

--- src/main/scala/leetcode/DesignHitCounter.scala
+++ src/main/scala/leetcode/DesignHitCounter.scala
@@ -1,26 +1,13 @@
 package leetcode
 
-import scala.collection.mutable
-
 class HitCounter() {
-  val queue = mutable.Queue[Hit]()
-  case class Hit(timestamp: Int)
 
-  def hit(timestamp: Int) {
-    queue.enqueue(Hit(timestamp))
+  def hit(timestamp: Int): Unit = {
 
   }
 
   def getHits(timestamp: Int): Int = {
-    if (queue.isEmpty) {
-      0
-    } else {
-      while (queue.nonEmpty && timestamp - 300 >= queue.head.timestamp) {
-        queue.dequeue()
-      }
 
-      queue.length
-    }
   }
 
 }
