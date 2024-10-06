75001850a3d47247c587ecba85eddf47aa2202cb
Žan Žibert <zzibert@plume.com>
2024-10-06 09:56:13 +0200

    LRU Cache #146 Redone

--- src/main/scala/leetcode/LRUCache.scala
+++ src/main/scala/leetcode/LRUCache.scala
@@ -3,54 +3,49 @@ package leetcode
 import scala.collection.mutable
 
 class LRUCache(_capacity: Int) {
-    val cache = mutable.Map[Int, (Int, Int)]()
-    val used = mutable.ArrayBuffer[(Int, Int)]()
-    var counter = 0
+  val cache = mutable.Map[Int, (Int, Int)]()
+  var usage = mutable.ListBuffer[(Int, Int)]()
+
   def get(key: Int): Int = {
     cache.get(key) match {
-      case Some((value, _)) =>
-        cache += (key -> (value, counter))
-        used.addOne((key, counter))
-        counter += 1
+      case Some((value, counter)) =>
+        increment(key, value, counter)
+
         value
+
       case None =>
         -1
     }
   }
 
-  def put(key: Int, value: Int) {
-    cache += (key -> (value, counter))
-    used.addOne((key, counter))
-    counter += 1
+  def put(key: Int, value: Int): Unit = {
+    cache.get(key) match {
+      case Some((_, counter)) =>
+        increment(key, value, counter)
 
-    if (cache.size > _capacity) {
-      val leastUsedKey = findLeastUsedKey(cache, used)
-      cache -= leastUsedKey
+      case None =>
+        if (cache.size == _capacity) {
+          var foundLeastRecentlyUsed = false
+          while (!foundLeastRecentlyUsed) {
+            val node = usage.head
+            val (usageKey, usageCounter) = node
+            if (usageCounter == cache(usageKey)._2) {
+              foundLeastRecentlyUsed = true
+              cache.remove(usageKey)
+              increment(key, value, 0)
+            }
+            usage.dropInPlace(1)
+          }
+        } else {
+          increment(key, value, 0)
+        }
+
     }
   }
 
-  def findLeastUsedKey(cache: mutable.Map[Int, (Int, Int)], used: mutable.ArrayBuffer[(Int, Int)]): Int = {
-    for (i <- 0 until used.length) {
-      val (usedKey, usedCounter) = used(i)
-      cache.get(usedKey) match {
-        case Some((_, counter)) =>
-          if (counter == usedCounter) {
-            used.dropInPlace(i+1)
-            return usedKey
-          }
-
-        case _ =>
-      }
-    }
-
-    0
+  def increment(key: Int, value: Int, counter: Int): Unit = {
+    cache.update(key, (value, counter + 1))
+    usage.addOne((key, counter + 1))
   }
 
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * var obj = new LRUCache(capacity)
- * var param_1 = obj.get(key)
- * obj.put(key,value)
- */
