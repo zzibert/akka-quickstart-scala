27a35549670fa571d5ca36a7639608dd0b0e7369
Žan Žibert <zzibert@plume.com>
2025-01-18 12:39:33 +0100

    UniquePaths redone

--- src/main/scala/leetcode/UniquePaths.scala
+++ src/main/scala/leetcode/UniquePaths.scala
@@ -1,9 +1,28 @@
 package leetcode
 
-import scala.collection.mutable
-
 object Solution {
   def uniquePaths(m: Int, n: Int): Int = {
+    val grid: Array[Array[Int]] = Array.ofDim(m, n)
+    grid(0)(0) = 1
 
+    for {
+      i <- 0 until m
+      j <- 0 until n
+      if !(i == 0 && i == j)
+    } {
+      val upBlock = getBlockPaths(i - 1, j, grid)
+      val leftBlock = getBlockPaths(i, j - 1, grid)
+      grid(i)(j) = upBlock + leftBlock
+    }
+
+    grid(m-1)(n-1)
+  }
+
+  def getBlockPaths(m: Int, n: Int, grid: Array[Array[Int]]): Int = {
+    if (m >= 0 && n >= 0) {
+      grid(m)(n)
+    } else {
+      0
+    }
   }
 }
