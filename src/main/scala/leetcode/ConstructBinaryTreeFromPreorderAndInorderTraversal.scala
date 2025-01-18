ab31861d64ced8ff5844d77757cbfd681fe9f12b
Žan Žibert <zzibert@plume.com>
2025-01-18 12:47:00 +0100

    reset 105

--- src/main/scala/leetcode/ConstructBinaryTreeFromPreorderAndInorderTraversal.scala
+++ src/main/scala/leetcode/ConstructBinaryTreeFromPreorderAndInorderTraversal.scala
@@ -1,26 +1,7 @@
 package leetcode
 
-object ConstructBinaryTreeFromPreorderAndInorderTraversal {
+object Solution {
   def buildTree(preorder: Array[Int], inorder: Array[Int]): TreeNode = {
-    if (inorder.isEmpty || preorder.isEmpty) {
-      null
-    } else {
-      val middleOption = preorder.find(inorder.contains(_))
 
-      middleOption match {
-        case Some(middle) =>
-          val root = new TreeNode(middle)
-
-          val middleIndexPreorder = preorder.indexWhere(_ == middle)
-          val middleIndexInorder = inorder.indexWhere(_ == middle)
-
-          root.left = buildTree(preorder.drop(middleIndexPreorder+1), inorder.take(middleIndexInorder))
-          root.right = buildTree(preorder.drop(middleIndexPreorder+1), inorder.drop(middleIndexInorder+1))
-
-          root
-        case None =>
-          null
-      }
-    }
   }
 }
