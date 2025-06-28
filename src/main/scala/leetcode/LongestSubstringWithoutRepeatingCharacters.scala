e76bffa4befa32baf4047ef54eb831c6051cbb31
Žan Žibert <zzibert@plume.com>
2025-06-28 17:06:28 +0200

    Revert "Revert "reset #3""
    This reverts commit 38a76c69db6ab4c550a9d3423dee4d499aef1ce1.

--- src/main/scala/leetcode/LongestSubstringWithoutRepeatingCharacters.scala
+++ src/main/scala/leetcode/LongestSubstringWithoutRepeatingCharacters.scala
@@ -1,41 +1,5 @@
-package leetcode
-
-import scala.collection.mutable
-
 object Solution {
   def lengthOfLongestSubstring(s: String): Int = {
-    val array = s.toCharArray
-    var maxLength = 0
-    var i = 0
-    var isNotDetermined = true
-    while (isNotDetermined && i < s.length) {
-      var j = i
-      var isUnique = true
-      var currentLength = 0
-      val characters = mutable.Map[Char, Boolean]()
-      while (isUnique && j < s.length) {
-        val char = array(j)
-        characters.get(char) match {
-          case Some(_) => isUnique = false
-          case None =>
-            characters.update(char, true)
-            currentLength += 1
-        }
 
-        j += 1
-      }
-
-      if (currentLength > maxLength) {
-        maxLength = currentLength
-      }
-
-      i += 1
-
-      if (currentLength >= (s.length - i)) {
-        isNotDetermined = false
-      }
-    }
-
-    maxLength
   }
 }
