1610cc86714bd101d38d8302f4471dc0c6f34982
Žan Žibert <zzibert@plume.com>
2025-06-28 23:49:46 +0200

    Reset binary tree level order traversal

--- src/main/scala/leetcode/BinaryTreeLevelOrderTraversal.scala
+++ src/main/scala/leetcode/BinaryTreeLevelOrderTraversal.scala
@@ -1,40 +1,5 @@
-package leetcode
-
-import scala.collection.mutable
-
-/**
-  * Definition for a binary tree node.
-  * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
-  *   var value: Int = _value
-  *   var left: TreeNode = _left
-  *   var right: TreeNode = _right
-  * }
-  */
 object Solution {
   def levelOrder(root: TreeNode): List[List[Int]] = {
-    val queue = mutable.Queue[TreeNode]()
 
-    var result = List[List[Int]]()
-
-    queue.enqueue(root)
-
-    while (queue.nonEmpty) {
-      var level = List[Int]()
-      val length = queue.length
-      for (_ <- 0 until length) {
-        val node = queue.dequeue()
-        if (node != null) {
-          level = level :+ node.value
-          queue.enqueue(node.left)
-          queue.enqueue(node.right)
-        }
-      }
-
-      if (level.nonEmpty) {
-        result = result :+ level
-      }
-    }
-
-    result
   }
 }
--- src/main/scala/leetcode/ThreeSum.scala
+++ src/main/scala/leetcode/ThreeSum.scala
@@ -1,5 +1,17 @@
 object Solution {
   def threeSum(nums: Array[Int]): List[List[Int]] = {
+    var result = Set[List[Int]]()
 
+    for {
+      i <- 0 until nums.length
+      j <- i + 1 until nums.length
+      k <- j + 1 until nums.length
+    } {
+      if (nums(i) + nums(j) + nums(k) == 0) {
+        result += List(nums(i), nums(j), nums(k)).sorted
+      }
+    }
+
+    result.toList
   }
 }
