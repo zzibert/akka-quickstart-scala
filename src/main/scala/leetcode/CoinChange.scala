f80ca67e2393fb0e6e0101259a00bb641a8d6748
Žan Žibert <zzibert@plume.com>
2025-06-29 15:53:25 +0200

    Reset rotting oranges

--- src/main/scala/leetcode/CoinChange.scala
+++ src/main/scala/leetcode/CoinChange.scala
@@ -1,5 +1,25 @@
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
 object Solution {
   def coinChange(coins: Array[Int], amount: Int): Int = {
+    val currentBest: mutable.ListBuffer[Int] = ListBuffer.empty
 
+  }
+
+  def coinChangeHelper(
+      amount: Int,
+      iterations: Int,
+      coins: Array[Int],
+      currentBest: ListBuffer[Int],
+  ): Int = {
+    if (amount == 0) {
+      currentBest.headOption match {
+        case Some(current) =>
+          if (current > iterations) {
+            currentBest = ListBuffer(iterations)
+          }
+      }
+    }
   }
 }
