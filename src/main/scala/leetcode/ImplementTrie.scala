6d82f5ebc7ad24f6a9eb5eb4f2be7ba946531585
Žan Žibert <zzibert@plume.com>
2025-06-29 15:22:41 +0200

    Reset implement Trie

--- src/main/scala/leetcode/ImplementTrie.scala
+++ src/main/scala/leetcode/ImplementTrie.scala
@@ -1,68 +1,15 @@
-package leetcode
-
 class Trie() {
-  val root = new Node(' ')
 
   def insert(word: String): Unit = {
-    val array = word.toCharArray
-    root.insert(array)
+
   }
 
   def search(word: String): Boolean = {
-    val array = word.toCharArray
-    root.search(array)
+
   }
 
   def startsWith(prefix: String): Boolean = {
-    val array = prefix.toCharArray
-    root.startsWith(array)
+
   }
 
-}
-
-class Node(val char: Char) {
-  var isWord: Boolean = false
-  var children: Seq[Node] = Seq.empty
-
-  def insert(word: Array[Char]): Unit = {
-    if (word.isEmpty) {
-      isWord = true
-    } else {
-      children.find(child => child.char == word.head) match {
-        case Some(child) =>
-          child.insert(word.tail)
-
-        case None =>
-          val child = new Node(word.head)
-          children = children :+ child
-          child.insert(word.tail)
-      }
-    }
-  }
-
-  def search(word: Array[Char]): Boolean = {
-    if (word.isEmpty) {
-      isWord
-    } else {
-      children.find(child => child.char == word.head) match {
-        case Some(child) =>
-          child.search(word.tail)
-
-        case None =>
-          false
-      }
-    }
-  }
-
-  def startsWith(prefix: Array[Char]): Boolean = {
-    if (prefix.size == 1) {
-      children.exists(child => child.char == prefix.head)
-    } else {
-      children.find(child => child.char == prefix.head) match {
-        case Some(child) => child.startsWith(prefix.tail)
-
-        case None => false
-      }
-    }
-  }
 }
