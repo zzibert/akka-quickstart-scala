3f577a04b51f63ae560a0b8a6a7ce1c13513556d
Žan Žibert <zzibert@plume.com>
2025-06-29 17:55:25 +0200

    Redo number of islands

--- src/main/scala/leetcode/NumberOfIslands.scala
+++ src/main/scala/leetcode/NumberOfIslands.scala
@@ -1,5 +1,40 @@
 object Solution {
   def numIslands(grid: Array[Array[Char]]): Int = {
+    var result = 0
+    val length = grid.length
+    val width = grid(0).length
 
+    for {
+      x <- 0 until length
+      y <- 0 until width
+    } {
+      if (grid(x)(y) == '1') {
+        result += 1
+        sunk(x, y, grid)
+      }
+    }
+
+    result
+  }
+
+  def isWithinBounds(x: Int, y: Int, length: Int, width: Int): Boolean =
+    x >= 0 && x < length && y >= 0 && y < width
+
+  def sunk(x: Int, y: Int, grid: Array[Array[Char]]): Unit = {
+    val length = grid.length
+    val width = grid(0).length
+    val directions = List((-1, 0), (0, 1), (1, 0), (0, -1))
+
+    grid(x)(y) = '0'
+
+    for {
+      direction <- directions
+    } {
+      val newX = x + direction._1
+      val newY = y + direction._2
+      if (isWithinBounds(newX, newY, length, width) && grid(newX)(newY) == '1') {
+        sunk(newX, newY, grid)
+      }
+    }
   }
 }
