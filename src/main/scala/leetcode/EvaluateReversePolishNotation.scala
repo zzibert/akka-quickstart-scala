352f55f0a7ffe96a0486c9dc2c1fde81800a195f
Žan Žibert <zzibert@plume.com>
2025-06-29 14:39:24 +0200

    reset Reverse polish notation

--- src/main/scala/leetcode/EvaluateReversePolishNotation.scala
+++ src/main/scala/leetcode/EvaluateReversePolishNotation.scala
@@ -1,45 +1,5 @@
-package leetcode
-
-import scala.collection.mutable
-
 object Solution {
   def evalRPN(tokens: Array[String]): Int = {
-    val stack = mutable.Stack[Int]()
 
-    tokens foreach { token =>
-      token match {
-        case "+" =>
-          val (number2, number1) = getNumbers(stack)
-          val result = number1 + number2
-          stack.push(result)
-
-        case "-" =>
-          val (number2, number1) = getNumbers(stack)
-          val result = number1 - number2
-          stack.push(result)
-
-        case "*" =>
-          val (number2, number1) = getNumbers(stack)
-          val result = number1 * number2
-          stack.push(result)
-
-        case "/" =>
-          val (number2, number1) = getNumbers(stack)
-          val result = number1 / number2
-          stack.push(result)
-
-        case numberString =>
-          val number = Integer.parseInt(numberString, 10)
-          stack.push(number)
-      }
-    }
-
-    stack.pop()
-  }
-
-  def getNumbers(stack: mutable.Stack[Int]): (Int, Int) = {
-    val n1 = stack.pop()
-    val n2 = stack.pop()
-    (n1, n2)
   }
 }
