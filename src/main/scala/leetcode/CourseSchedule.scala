8c274640505e53e1dd33818b196e631404acb590
Žan Žibert <zzibert@plume.com>
2025-06-29 15:22:07 +0200

    Redo course schedule

--- src/main/scala/leetcode/CourseSchedule.scala
+++ src/main/scala/leetcode/CourseSchedule.scala
@@ -1,5 +1,39 @@
+import scala.collection.mutable
+
 object Solution {
   def canFinish(numCourses: Int, prerequisites: Array[Array[Int]]): Boolean = {
+    val availableCourses = mutable.Set[Int]()
 
+    var requiredCourses =
+      prerequisites
+        .groupBy(x => x(0))
+        .collect {
+          case (key, value) =>
+            val courses = value.map(x => x(1))
+            key -> courses
+        }
+
+    for (i <- 0 until numCourses) {
+      if (requiredCourses.get(i).isEmpty) {
+        availableCourses.add(i)
+      }
+    }
+
+    var proceed = true
+
+    while (proceed) {
+      proceed = false
+
+      requiredCourses foreach {
+        case (course, courses) =>
+          if (courses.forall(availableCourses.contains)) {
+            availableCourses.add(course)
+            requiredCourses -=  course
+            proceed = true
+          }
+      }
+    }
+
+    requiredCourses.isEmpty
   }
 }
