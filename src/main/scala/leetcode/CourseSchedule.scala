062ad1981294e769784de2c2e10f7bd0c21180d1
Žan Žibert <zzibert@plume.com>
2025-06-29 14:51:14 +0200

    Reset course schedule

--- src/main/scala/leetcode/CourseSchedule.scala
+++ src/main/scala/leetcode/CourseSchedule.scala
@@ -1,46 +1,5 @@
-package leetcode
-
-import scala.collection.mutable
-
 object Solution {
   def canFinish(numCourses: Int, prerequisites: Array[Array[Int]]): Boolean = {
-    val classes = prerequisites.groupBy(_(0)) map {
-      case (course, required) =>
-        val requiredClasses = required.map(_(1))
-        (course, requiredClasses)
-    }
 
-    val finishedClasses = mutable.Set[Int]()
-    val queue = mutable.Queue[Int]()
-
-    for (c <- 0 until numCourses) {
-      queue.enqueue(c)
-    }
-
-    var foundNewClass = true
-
-    while (queue.nonEmpty && foundNewClass) {
-      val counter = queue.size
-      foundNewClass = false
-      for (_ <- 0 until counter) {
-        val unfinished = queue.dequeue()
-        classes.get(unfinished) match {
-          case Some(required) =>
-            if (required.forall(finishedClasses.contains)) {
-              finishedClasses.add(unfinished)
-              foundNewClass = true
-            } else {
-              queue.enqueue(unfinished)
-            }
-
-          case None =>
-            foundNewClass = true
-            finishedClasses.add(unfinished)
-
-        }
-      }
-    }
-
-    queue.isEmpty
   }
 }
