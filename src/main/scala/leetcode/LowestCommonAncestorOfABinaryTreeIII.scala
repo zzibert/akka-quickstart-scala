00b24951571395601121e3101232eda205b43fdb
Žan Žibert <zzibert@plume.com>
2025-07-05 14:30:26 +0200

    Redo #236 Lowest Common ancestor of a binary tree

--- src/main/scala/leetcode/LowestCommonAncestorOfABinaryTreeIII.scala
+++ src/main/scala/leetcode/LowestCommonAncestorOfABinaryTreeIII.scala
@@ -1,5 +1,34 @@
 object Solution {
-  def lowestCommonAncestor(root: TreeNode, p: TreeNode, q: TreeNode): TreeNode = {
+  def lowestCommonAncestor(root: TreeNode,
+                           p: TreeNode,
+                           q: TreeNode): TreeNode = {
 
+    val pPath = getPath(root, p.value, List.empty[TreeNode])
+    val qPath = getPath(root, q.value, List.empty[TreeNode])
+
+    pPath.find(node => qPath.contains(node)).get
+  }
+
+  def getPath(root: TreeNode,
+              target: Int,
+              path: List[TreeNode]): List[TreeNode] = {
+    if (root != null) {
+      if (root.value == target) {
+        root :: path
+      } else {
+        val rightPath = getPath(root.right, target, root :: path)
+        val leftPath = getPath(root.left, target, root :: path)
+        val children = List(rightPath, leftPath).filter(_.nonEmpty)
+
+        if (children.isEmpty) {
+          List.empty
+        } else {
+          children.head
+        }
+      }
+
+    } else {
+      List.empty
+    }
   }
 }
