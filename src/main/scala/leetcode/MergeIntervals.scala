c26dd005b731b841009675783efdeafe965b7d49
Žan Žibert <zzibert@plume.com>
2025-07-05 16:59:22 +0200

    Redo #56 Merge Intervals

--- src/main/scala/leetcode/MergeIntervals.scala
+++ src/main/scala/leetcode/MergeIntervals.scala
@@ -1,5 +1,27 @@
+import scala.collection.mutable
+
 object Solution {
   def merge(intervals: Array[Array[Int]]): Array[Array[Int]] = {
+    var i = 0
+    val result: mutable.ArrayBuffer[Array[Int]] =
+      mutable.ArrayBuffer.from(intervals).sortBy(_(0))
 
+    while (i < result.length) {
+      val j = i + 1
+      if (j < result.length) {
+        if (result(i)(1) >= result(j)(0)) {
+          val start = Math.min(result(i)(0), result(j)(0))
+          val end = Math.max(result(i)(1), result(j)(1))
+          result(i) = Array(start, end)
+          result.remove(j)
+        } else {
+          i += 1
+        }
+      } else {
+        i += 1
+      }
+    }
+
+    result.toArray
   }
 }
