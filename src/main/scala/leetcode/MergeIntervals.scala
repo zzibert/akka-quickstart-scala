64bbee31fe236decd0dd6cc92b9a6f8ba2fab2c5
Žan Žibert <zzibert@plume.com>
2025-07-05 16:24:57 +0200

    Clear merge intervals

--- src/main/scala/leetcode/MergeIntervals.scala
+++ src/main/scala/leetcode/MergeIntervals.scala
@@ -1,28 +1,5 @@
-package leetcode
-
-import scala.collection.mutable
-
 object Solution {
   def merge(intervals: Array[Array[Int]]): Array[Array[Int]] = {
-    var i = 0
-    val result = mutable.ArrayBuffer[Array[Int]]()
-    val sortedIntervals = intervals.sortBy(_(0))
 
-    while (i < sortedIntervals.length) {
-      var interval = sortedIntervals(i)
-      var j = i + 1
-      while (j < intervals.length && sortedIntervals(j)(0) <= interval(1)) {
-        val bigger = Math.max(interval(1), sortedIntervals(j)(1))
-        val smaller = Math.min(interval(0), sortedIntervals(j)(0))
-        interval = Array(smaller, bigger)
-        i += 1
-        j += 1
-      }
-
-      result.addOne(interval)
-      i += 1
-    }
-
-    result.toArray
   }
 }
--- src/main/scala/leetcode/SortColors.scala
+++ src/main/scala/leetcode/SortColors.scala
@@ -1,5 +1,35 @@
 object Solution {
   def sortColors(nums: Array[Int]): Unit = {
+    var redIndex = 0
+    var whiteIndex = 0
+    var blueIndex = 0
 
+    var i = 0
+
+    while (i < nums.length) {
+      val number = nums(i)
+      number match {
+        case 0 =>
+          if (i == redIndex) {
+            redIndex += 1
+            whiteIndex = redIndex + 1
+            i += 1
+          } else {
+            val temp = nums(redIndex)
+            nums(redIndex) = 0
+            nums(i) = temp
+            redIndex += 1
+          }
+
+        case 1 =>
+          if (i == whiteIndex) {
+            whiteIndex
+          }
+
+        case 2 =>
+          i += 1
+
+      }
+    }
   }
 }
