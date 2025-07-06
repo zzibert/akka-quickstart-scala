2745dc404dcdce977b8d762bb306f38a8affe245
Žan Žibert <zzibert@plume.com>
2025-07-06 14:08:26 +0200

    Revert "Redo #11 Container with most water"
    This reverts commit 00f1f5488daa55e7d9c6876d437d261dbb243cfa.

--- src/main/scala/leetcode/ContainerWithMostWater.scala
+++ src/main/scala/leetcode/ContainerWithMostWater.scala
@@ -1,29 +1,5 @@
 object Solution {
   def maxArea(height: Array[Int]): Int = {
-    var maxWater = 0
-    var currentHeight = 0
-    var i = 0
-    while (i < height.length) {
-      val leftIndex = i
-      val leftHeight = height(i)
-      var rightIndex = i + 1
 
-      if (leftHeight > currentHeight) {
-        currentHeight = leftHeight
-        while (rightIndex < height.length) {
-          val rightHeight = height(rightIndex)
-          val yAxis = Math.min(leftHeight, rightHeight)
-          val waterAmount = yAxis * (rightIndex - leftIndex)
-          if (waterAmount > maxWater) {
-            maxWater = waterAmount
-          }
-          rightIndex += 1
-        }
-      } else {
-        i += 1
-      }
-    }
-
-    maxWater
   }
 }
