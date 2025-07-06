78fea1382206d63a2e523f597dabfa0e2ae1df7a
Žan Žibert <zzibert@plume.com>
2025-07-06 10:32:37 +0200

    Redo #46 permutations

--- src/main/scala/leetcode/Permutations.scala
+++ src/main/scala/leetcode/Permutations.scala
@@ -1,5 +1,26 @@
+
+
 object Solution {
   def permute(nums: Array[Int]): List[List[Int]] = {
 
+    createPermutation(nums, List.empty)
+  }
+
+  def createPermutation(nums: Array[Int],
+                        permutation: List[Int]): List[List[Int]] = {
+    if (nums.isEmpty) {
+      List(permutation)
+    } else {
+      var result: List[List[Int]] = List.empty
+
+      for (i <- 0 until nums.length) {
+        val number = nums(i)
+        val newNums = nums.take(i) ++ nums.drop(i + 1)
+        val child = createPermutation(newNums, number :: permutation)
+        result = child ++ result
+      }
+
+      result.distinct
+    }
   }
 }
