5d83e91c0fd822afd17aa3a8d16aa8080d5ea4d9
Žan Žibert <zzibert@plume.com>
2025-07-06 14:08:28 +0200

    Revert "Clear containers"
    This reverts commit 0527ffd8cf2a13b28b06e70d87d4eb618c019624.

--- src/main/scala/leetcode/ContainerWithMostWater.scala
+++ src/main/scala/leetcode/ContainerWithMostWater.scala
@@ -1,5 +1,28 @@
-object Solution {
-  def maxArea(height: Array[Int]): Int = {
+package leetcode
 
+object ContainerWithMostWater {
+  def maxArea(height: Array[Int]): Int = {
+    var first = 0
+    var second = 1
+
+    var tallestFirst = 0
+    var maxAmount = 0
+
+    for {
+      i <- 0 until (height.length-1)
+    } {
+      if (height(i) > tallestFirst) {
+        tallestFirst = height(i)
+        for (j <- i+1 until height.length) {
+          val localHight = Math.min(tallestFirst, height(j))
+          val localSum = localHight * (j - i)
+          if (localSum > maxAmount) {
+            maxAmount = localSum
+          }
+        }
+      }
+    }
+
+    maxAmount
   }
 }
