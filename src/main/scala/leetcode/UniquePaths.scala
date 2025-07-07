6010d32cdac01fb2c8849b44b09b473683da8e96
Žan Žibert <zzibert@plume.com>
2025-07-07 16:58:18 +0200

    Redo 62. Unique Paths

--- src/main/scala/leetcode/UniquePaths.scala
+++ src/main/scala/leetcode/UniquePaths.scala
@@ -1,5 +1,19 @@
+import scala.collection.mutable
+
 object Solution {
   def uniquePaths(m: Int, n: Int): Int = {
+    val paths = mutable.Map[(Int, Int), Int]()
+    paths.addOne((-1, 0), 1)
 
+    for {
+      i <- 0 until m
+      j <- 0 until n
+    } {
+      val down = paths.get((i - 1, j)).getOrElse(0)
+      val right = paths.get((i, j - 1)).getOrElse(0)
+      paths.addOne((i, j), down + right)
+    }
+
+    paths.get((m - 1, n - 1)).get
   }
 }
