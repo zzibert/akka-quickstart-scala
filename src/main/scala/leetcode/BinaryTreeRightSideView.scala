76452bf73e44d23a05fb2aa6d358e0ff38f61182
Žan Žibert <zzibert@plume.com>
2025-07-07 14:49:31 +0200

    Clear Binary Tree right side view

--- src/main/scala/leetcode/BinaryTreeRightSideView.scala
+++ src/main/scala/leetcode/BinaryTreeRightSideView.scala
@@ -1,30 +1,5 @@
-package leetcode
-
-import scala.collection.mutable
-
 object Solution {
   def rightSideView(root: TreeNode): List[Int] = {
-    val result = mutable.ArrayBuffer[Int]()
-    val queue = mutable.Queue[TreeNode]()
 
-    queue.enqueue(root)
-
-    while (queue.nonEmpty) {
-      val level = mutable.ArrayBuffer[Int]()
-      for (_ <- 0 until queue.length) {
-        val node = queue.dequeue()
-        if (node != null) {
-          level.addOne(node.value)
-          queue.enqueue(node.left)
-          queue.enqueue(node.right)
-        }
-      }
-      if (level.nonEmpty) {
-        result.addOne(level.last)
-      }
-    }
-
-    result.toList
   }
 }
-
