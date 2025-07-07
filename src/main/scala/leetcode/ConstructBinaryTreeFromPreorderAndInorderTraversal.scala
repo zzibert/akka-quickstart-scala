be6475dd02370cc6540c595a76e4d0476b2cc4b3
Žan Žibert <zzibert@plume.com>
2025-07-07 15:32:17 +0200

    Redo #105 Construct binary tree from Preorder and Inorder Traversal

--- src/main/scala/leetcode/ConstructBinaryTreeFromPreorderAndInorderTraversal.scala
+++ src/main/scala/leetcode/ConstructBinaryTreeFromPreorderAndInorderTraversal.scala
@@ -1,5 +1,23 @@
 object Solution {
-  def buildTree(preorder: Array[Int], inorder: Array[Int]): TreeNode = {
 
+  def buildTree(preorder: Array[Int], inorder: Array[Int]): TreeNode = {
+    if (preorder.isEmpty) {
+      null
+    } else {
+      preorder.find(inorder.contains) match {
+        case Some(root) =>
+          val node = new TreeNode(root)
+          val preorderIndex = preorder.indexOf(root)
+          val inorderIndex = inorder.indexOf(root)
+          node.left =
+            buildTree(preorder.drop(preorderIndex), inorder.take(inorderIndex))
+          node.right = buildTree(preorder.drop(preorderIndex),
+                                 inorder.drop(inorderIndex + 1))
+          node
+
+        case None =>
+          null
+      }
+    }
   }
 }
