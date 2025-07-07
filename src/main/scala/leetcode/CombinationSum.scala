f7594191e5e4aef30dcecabd01a0946f3d5d7b54
Žan Žibert <zzibert@plume.com>
2025-07-07 14:48:43 +0200

    Redo #39 Combination Sum

--- src/main/scala/leetcode/CombinationSum.scala
+++ src/main/scala/leetcode/CombinationSum.scala
@@ -1,5 +1,41 @@
+import scala.collection.mutable
+
 object Solution {
   def combinationSum(candidates: Array[Int], target: Int): List[List[Int]] = {
+    val solutions = mutable.Map[(Int, List[Int]), Boolean]()
+    combinationSumHelper(candidates, target, List.empty, solutions).toList
+  }
 
+  def combinationSumHelper(
+      candidates: Array[Int],
+      target: Int,
+      current: List[Int],
+      solutions: mutable.Map[(Int, List[Int]), Boolean],
+  ): Set[List[Int]] = {
+    solutions.addOne((target, current), true)
+    if (target == 0) {
+      Set(current)
+    } else if (target > 0) {
+      val result =
+        for {
+          i <- 0 until candidates.length
+        } yield {
+          val number = candidates(i)
+          val candidate = (number :: current).sorted
+          if (solutions.get(target - number, candidate).isEmpty) {
+            combinationSumHelper(
+              candidates = candidates,
+              target = target - number,
+              current = candidate,
+              solutions = solutions
+            )
+          } else {
+            List.empty
+          }
+        }
+      result.flatten.toSet
+    } else {
+      Set.empty
+    }
   }
 }
