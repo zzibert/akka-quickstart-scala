47ce7f428effc2d6d961760156e797a8b5a18801
Žan Žibert <zzibert@plume.com>
2025-07-26 17:23:53 +0200

    LRU redone

--- src/main/scala/leetcode/LRUCache.scala
+++ src/main/scala/leetcode/LRUCache.scala
@@ -1,51 +1,50 @@
-package leetcode
-
 import scala.collection.mutable
 
 class LRUCache(_capacity: Int) {
-  val cache = mutable.Map[Int, (Int, Int)]()
-  var usage = mutable.ListBuffer[(Int, Int)]()
+  val entries = mutable.Map[Int, (Int, Int)]()
+  val used = mutable.ArrayBuffer[(Int, Int)]()
 
   def get(key: Int): Int = {
-    cache.get(key) match {
-      case Some((value, counter)) =>
-        increment(key, value, counter)
-
+    entries.get(key) match {
+      case Some(tuple) =>
+        val value = tuple._1
+        val counter = tuple._2 + 1
+        used.addOne((key, counter))
+        entries.update(key, (value, counter))
         value
 
-      case None =>
-        -1
+      case None => -1
     }
   }
 
   def put(key: Int, value: Int): Unit = {
-    cache.get(key) match {
-      case Some((_, counter)) =>
-        increment(key, value, counter)
+    entries.get(key) match {
+      case Some((_, currentCounter)) =>
+        val newCounter = currentCounter + 1
+        entries.update(key, (value, newCounter))
+        used.addOne((key, newCounter))
 
       case None =>
-        if (cache.size == _capacity) {
-          var foundLeastRecentlyUsed = false
-          while (!foundLeastRecentlyUsed) {
-            val node = usage.head
-            val (usageKey, usageCounter) = node
-            if (usageCounter == cache(usageKey)._2) {
-              foundLeastRecentlyUsed = true
-              cache.remove(usageKey)
-              increment(key, value, 0)
-            }
-            usage.dropInPlace(1)
-          }
-        } else {
-          increment(key, value, 0)
-        }
+        entries.addOne(key, (value, 0))
+        used.addOne((key, 0))
 
+        if (entries.size > _capacity) {
+          var i = 0
+          var condition = false
+          while (!condition && i < entries.size) {
+            val (storedKey, storedCounter) = used(i)
+            if (entries
+                  .get(storedKey)
+                  .exists(tuple => tuple._2 == storedCounter)) {
+              condition = true
+            } else {
+              i += 1
+            }
+          }
+          val dropKey = used(i)._1
+          entries.remove(dropKey)
+          used.dropInPlace(i + 1)
+        }
     }
   }
-
-  def increment(key: Int, value: Int, counter: Int): Unit = {
-    cache.update(key, (value, counter + 1))
-    usage.addOne((key, counter + 1))
-  }
-
 }
