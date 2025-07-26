c6d094a9bf972648019da27acf6b7e0609842d2e
Žan Žibert <zzibert@plume.com>
2025-07-26 17:47:16 +0200

    Redo house robber #198

--- src/main/scala/leetcode/HouseRobber.scala
+++ src/main/scala/leetcode/HouseRobber.scala
@@ -1,5 +1,24 @@
+
 object Solution {
   def rob(nums: Array[Int]): Int = {
+    val loots = Array.fill[Int](nums.length)(0)
 
+    if (nums.length < 2) {
+      nums(0)
+    } else if (nums.length < 3) {
+      Math.max(nums(0), nums(1))
+    } else if (nums.length < 4) {
+      Math.max(nums(0) + nums(2), nums(1))
+    } else {
+      loots(0) = nums(0)
+      loots(1) = nums(1)
+      loots(2) = nums(2) + nums(0)
+      for (i <- 3 until nums.length) {
+        val left = loots(i - 3)
+        val right = loots(i - 2)
+        loots(i) = nums(i) + Math.max(left, right)
+      }
+      Math.max(loots(nums.size - 2), loots(nums.length - 1))
+    }
   }
 }
