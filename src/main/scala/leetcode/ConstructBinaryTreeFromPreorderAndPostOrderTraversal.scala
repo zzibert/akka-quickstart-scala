060d815ba5be4194b93f56e1bf287dd80338369f
Žan Žibert <zzibert@plume.com>
2025-07-26 17:29:27 +0200

    Redo house robber

--- src/main/scala/leetcode/ConstructBinaryTreeFromPreorderAndPostOrderTraversal.scala
+++ src/main/scala/leetcode/ConstructBinaryTreeFromPreorderAndPostOrderTraversal.scala
@@ -1,3 +1,34 @@
-package leetcode class ConstructBinaryTreeFromPreorderAndPostOrderTraversal {
+import leetcode.TreeNode
 
+object Solution {
+
+  def constructFromPrePost(
+      preorder: Array[Int],
+      postorder: Array[Int]
+  ): TreeNode = {
+    if (preorder.isEmpty) {
+      null
+    } else {
+      val root = preorder.head
+      val node = new TreeNode(root)
+
+      if (preorder.tail.nonEmpty) {
+        val leftRoot = preorder.tail.head
+        val leftRootIndex = postorder.indexOf(leftRoot)
+        val leftPostorder = postorder.take(leftRootIndex)
+        val leftPreorder = Array(leftRoot) ++ leftPostorder
+        node.left = constructFromPrePost(leftPreorder, leftPostorder)
+      }
+
+      if (postorder.length > 1) {
+        val rightRoot = postorder(postorder.length-2)
+        val rightRootIndex = preorder.indexOf(rightRoot)
+        val rightPreorder = preorder.drop(rightRootIndex)
+        val rightPostorder = preorder.drop(rightRootIndex+1)
+        node.right = constructFromPrePost(rightPreorder, rightPostorder)
+      }
+
+      node
+    }
+  }
 }
--- src/main/scala/leetcode/SpiralMatrix.scala
+++ src/main/scala/leetcode/SpiralMatrix.scala
@@ -1,5 +1,25 @@
+import scala.collection.mutable
+
 object Solution {
   def spiralOrder(matrix: Array[Array[Int]]): List[Int] = {
+    val directions = Array((0, 1),(1, 0),(0, -1),(-1, 0))
+    val seen = mutable.Map[(Int, Int), Boolean]()
+    var result = mutable.ArrayBuffer[Int]()
+    var counters = 3
+    var x = 0
+    var y = 0
+    var length = matrix.length
+    var width = matrix(0).length
+    var index = 0
 
+    while (counters > 0) {
+      result.addOne(matrix(x)(y))
+
+    }
+
+  }
+
+  def isWithinBounds(x: Int, y: Int, length: Int, width: Int): Boolean = {
+    x >= 0 && x < length && y >= 0 && y < width
   }
 }
