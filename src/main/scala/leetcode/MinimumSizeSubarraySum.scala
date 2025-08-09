f42eeabd4bce8b368d72f5c66f159ed19ef34156
Žan Žibert <zzibert@plume.com>
2025-08-09 11:57:05 +0200

    Reset #209

--- src/main/scala/leetcode/MinimumSizeSubarraySum.scala
+++ src/main/scala/leetcode/MinimumSizeSubarraySum.scala
@@ -1,25 +1,5 @@
-package leetcode
-
-object MinimumSizeSubarraySum {
+object Solution {
   def minSubArrayLen(target: Int, nums: Array[Int]): Int = {
-    for (length <- 1 to nums.length) {
-      var summa = 0
-      for (i <- 0 until length) {
-        summa += nums(i)
-      }
 
-      for (i <- length until nums.length) {
-        if (summa >= target) {
-          return length
-        }
-        summa -= nums(i-length)
-        summa += nums(i)
-      }
-      if (summa >= target) {
-        return length
-      }
-    }
-
-    0
   }
 }
