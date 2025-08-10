87d01fcb66e3cc0205566b5c0e173fb3028fac30
Žan Žibert <zzibert@plume.com>
2025-08-10 11:41:20 +0200

    Redo #322 Coin Change

--- src/main/scala/leetcode/CoinChange.scala
+++ src/main/scala/leetcode/CoinChange.scala
@@ -1,25 +1,50 @@
 import scala.collection.mutable
-import scala.collection.mutable.ListBuffer
 
 object Solution {
   def coinChange(coins: Array[Int], amount: Int): Int = {
-    val currentBest: mutable.ListBuffer[Int] = ListBuffer.empty
+    val coinsBuffer = mutable.ArrayBuffer.from(coins)
+    val bestResultYet = mutable.Map[Int, Int]()
 
+    val result = coinChangeHelper(coinsBuffer, amount, 0, bestResultYet)
+
+    if (result == Int.MaxValue) {
+      -1
+    } else {
+      result
+    }
   }
 
-  def coinChangeHelper(
-      amount: Int,
-      iterations: Int,
-      coins: Array[Int],
-      currentBest: ListBuffer[Int],
-  ): Int = {
+  def coinChangeHelper(coins: mutable.ArrayBuffer[Int], amount: Int, numberOfCoins: Int, bestResultYet: mutable.Map[Int, Int]): Int = {
     if (amount == 0) {
-      currentBest.headOption match {
-        case Some(current) =>
-          if (current > iterations) {
-            currentBest = ListBuffer(iterations)
-          }
+      numberOfCoins
+    } else if (amount > 0) {
+      var result = Int.MaxValue
+
+      for {
+        coin <- coins
+      } {
+        val newAmount = amount - coin
+        val newNumberOfCoins = numberOfCoins + 1
+        bestResultYet.get(newAmount) match {
+          case Some(bestYet) =>
+            if (bestYet > newNumberOfCoins) {
+              val currentResult = coinChangeHelper(coins, amount - coin, numberOfCoins + 1, bestResultYet.addOne((newAmount, newNumberOfCoins)))
+              if (currentResult < result) {
+                result = currentResult
+              }
+            }
+
+          case None =>
+            val currentResult = coinChangeHelper(coins, amount - coin, numberOfCoins + 1, bestResultYet.addOne((newAmount, newNumberOfCoins)))
+            if (currentResult < result) {
+              result = currentResult
+            }
+        }
       }
+
+      result
+    } else {
+      Int.MaxValue
     }
   }
 }
--- src/main/scala/leetcode/LargestRectangleInHistogram.scala
+++ src/main/scala/leetcode/LargestRectangleInHistogram.scala
@@ -1,26 +1,48 @@
 package leetcode
 
+import scala.collection.mutable
+
 object Solution {
   def largestRectangleArea(heights: Array[Int]): Int = {
+    val stack = mutable.Stack[(Int, Int)]()
     var result = 0
-    heights.zipWithIndex foreach { case (height, index) =>
-      var currentValue = height
-      // Left Side
-      var leftIndex = index - 1
-      while (leftIndex >= 0 && heights(leftIndex) >= height) {
-        currentValue += height
-        leftIndex -= 1
-      }
 
-      // Right Side
-      var rightIndex = index + 1
-      while (rightIndex < heights.length && heights(rightIndex) >= height) {
-        currentValue += height
-        rightIndex += 1
-      }
+    heights.zipWithIndex foreach {
+      case (height, index) =>
 
+        if (stack.isEmpty) {
+          stack.push((height, index))
+        } else {
+          val (head, _) = stack.head
+          if (height > head) {
+            stack.push((height, index))
+          } else {
+            while (stack.nonEmpty && height < stack.head._1) {
+              val currentValue = height * (index+1 - stack.head._2)
+              if (currentValue > result) {
+                result = currentValue
+              }
+              stack.pop()
+            }
+
+            stack.push((height, index))
+          }
+        }
+    }
+
+    while (stack.nonEmpty) {
+      val index = heights.length
+      val currentValue = stack.head._1 * (index - stack.head._2)
       if (currentValue > result) {
         result = currentValue
+      }
+      val lastHead = stack.head._1
+      stack.pop()
+
+      if (stack.isEmpty) {
+        if (lastHead * index > result) {
+          result = lastHead * index
+        }
       }
     }
 
