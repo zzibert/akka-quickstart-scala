9bf25c597669c86682571af56c595f10464cb64b
Žan Žibert <zzibert@plume.com>
2025-08-10 13:17:41 +0200

    Retry #416 Partition Equal Subset Sum

--- src/main/scala/leetcode/PartitionEqualSubsetSum.scala
+++ src/main/scala/leetcode/PartitionEqualSubsetSum.scala
@@ -1,36 +1,7 @@
 package leetcode
 
-import scala.collection.mutable
-import scala.collection.mutable.ListBuffer
-
-class PartitionEqualSubsetSum {
+object Solution {
   def canPartition(nums: Array[Int]): Boolean = {
-    val result = ListBuffer[Int]()
-    val seen = mutable.Map[Int, Boolean]()
 
-
-    if (nums.sum % 2 != 0) {
-      return false
-    }
-
-    val target = nums.sum / 2
-
-    canPartitionHelper(nums, target, seen, result)
-
-    !result.isEmpty
-  }
-
-  def canPartitionHelper(nums: Array[Int], target: Int, seen: mutable.Map[Int, Boolean], result: ListBuffer[Int]): Unit = {
-    val summa = nums.sum
-    if (result.isEmpty && !seen.getOrElse(summa, false)) {
-      seen += (summa -> true)
-      if (summa == target) {
-        result.addOne(1)
-      } else if (summa > target) {
-        for (i <- 0 until nums.length) {
-          canPartitionHelper(nums.patch(i, Nil, 1), target, seen, result)
-        }
-      }
-    }
   }
 }
