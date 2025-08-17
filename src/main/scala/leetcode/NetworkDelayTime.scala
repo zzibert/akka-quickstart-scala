f3bea29fe70e5212a65bb27bb8971dc008f738f5
Žan Žibert <zzibert@plume.com>
2025-08-17 12:48:20 +0200

    Network Delay Time #743 - Djikstra Algorithm

--- src/main/scala/leetcode/NetworkDelayTime.scala
+++ src/main/scala/leetcode/NetworkDelayTime.scala
@@ -3,38 +3,48 @@ package leetcode
 import scala.collection.mutable
 
 object Solution {
-  case class Journey(node: Int, time: Int)
+  case class Journey(to: Int, time: Int)
 
   def networkDelayTime(times: Array[Array[Int]], n: Int, k: Int): Int = {
+    implicit val ordering: Ordering[Journey] = Ordering.by(_.time)
+    val queue = mutable.PriorityQueue[Journey]()(ordering.reverse)
+    val minimumTimeToNode = Array.fill(n)(Int.MaxValue)
+    minimumTimeToNode(k-1) = 0
 
-    val minimumTimePerNode = Array.fill(n)(Int.MaxValue)
-    minimumTimePerNode(k-1) = 0
-    val queue = mutable.Queue[Journey]()
+    val visited = Array.fill(n)(false)
 
     val destinations =
       times
         .groupBy(_(0))
-        .map { case (source, paths) =>
-          val toAndTime = paths.map(x => Array(x(1)-1, x(2)))
-          (source-1, toAndTime)
+        .map { case (source, trips) =>
+          val toAndTime = trips.map(x => Array(x(1)-1, x(2)))
+          (source - 1, toAndTime)
         }
 
     queue.enqueue(Journey(k-1, 0))
 
     while (queue.nonEmpty) {
       val journey = queue.dequeue()
+      visited(journey.to) = true
       for {
-        reachableNodes <- destinations.get(journey.node)
-        Array(to, time) <- reachableNodes
-        if minimumTimePerNode(to) > journey.time + time
+        journeys <- destinations.get(journey.to)
+        Array(to, time) <- journeys
+        if !visited(to)
       } {
-        minimumTimePerNode(to) = journey.time + time
+        if (minimumTimeToNode(to) > journey.time + time) {
+          minimumTimeToNode(to) = journey.time + time
+        }
+
         queue.enqueue(Journey(to, journey.time + time))
       }
     }
 
-    val maxDuration = minimumTimePerNode.max
+    val result = minimumTimeToNode.max
 
-    if (maxDuration == Int.MaxValue) -1 else maxDuration
+    if (result == Int.MaxValue) {
+      -1
+    } else {
+      result
+    }
   }
 }
