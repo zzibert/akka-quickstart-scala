54bded93ccbe7424c77f68a53d072d396367ebbc
Žan Žibert <zzibert@plume.com>
2025-08-23 16:45:57 +0200

    Asteroid Collision #735

---
+++ src/main/scala/leetcode/AsteroidCollision.scala
@@ -0,0 +1,42 @@
+package leetcode
+
+import scala.collection.mutable
+
+object Solution {
+  def asteroidCollision(asteroids: Array[Int]): Array[Int] = {
+    val rightTravellingAsteroids = mutable.Stack[(Int, Int)]()
+    val leftTravellingAsteroids = mutable.ListBuffer[(Int, Int)]()
+
+    for {
+      i <- 0 until asteroids.length
+      asteroid = asteroids(i)
+    } {
+      if (asteroid > 0) {
+        rightTravellingAsteroids.push((i, asteroid))
+      } else {
+        val convertedAsteroid = -asteroid
+        var destroyed = false
+        while (!destroyed && rightTravellingAsteroids.nonEmpty) {
+          if (rightTravellingAsteroids.head._2 <= convertedAsteroid) {
+            if (rightTravellingAsteroids.head._2 == convertedAsteroid) {
+              destroyed = true
+            }
+            rightTravellingAsteroids.pop()
+          } else {
+            destroyed = true
+          }
+        }
+        if (!destroyed) {
+          leftTravellingAsteroids.addOne((i, asteroid))
+        }
+      }
+    }
+
+    val result =
+      (rightTravellingAsteroids.toArray ++ leftTravellingAsteroids.toArray)
+        .sortBy(_._1)
+        .map(_._2)
+
+    result
+  }
+}
--- src/main/scala/leetcode/MinimumWindowSubstring.scala
+++ src/main/scala/leetcode/MinimumWindowSubstring.scala
@@ -2,6 +2,53 @@ package leetcode
 
 object Solution {
   def minWindow(s: String, t: String): String = {
+    var solution = ""
+    var targetCharacters = Map[Char, Int]()
+    val length = s.length
+    var slidingWindowSize = t.length
+    var found = false
 
+    t foreach { character =>
+      targetCharacters += (character, targetCharacters.getOrElse(character, 0) + 1)
+    }
+
+    while (!found && slidingWindowSize <= length) {
+      var currentCharacters = Map[Char, Int]()
+      for (j <- 0 until slidingWindowSize) {
+        currentCharacters += (s(j), currentCharacters.getOrElse(s(j), 0) + 1)
+      }
+
+      for {
+        i <- 0 until length - (slidingWindowSize - 1)
+        if !found
+      } {
+        if (isSolution(targetCharacters, currentCharacters)) {
+          found = true
+          solution = s.drop(i).take(slidingWindowSize)
+        } else {
+          val oldCharacter = s(i)
+          currentCharacters.get(oldCharacter) foreach { count =>
+            currentCharacters += (oldCharacter, count - 1)
+          }
+          if (i + slidingWindowSize < length) {
+            val newCharacter = s(i + slidingWindowSize)
+            currentCharacters += (newCharacter, currentCharacters.getOrElse(
+              newCharacter,
+              0) + 1)
+          }
+        }
+      }
+
+      slidingWindowSize += 1
+    }
+
+    solution
+  }
+
+  def isSolution(target: Map[Char, Int], current: Map[Char, Int]): Boolean = {
+    target forall {
+      case (character, count) =>
+        current.getOrElse(character, 0) >= count
+    }
   }
 }
